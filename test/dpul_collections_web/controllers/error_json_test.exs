499e6053965f3938394a9a5a034080e5e84841c6
Sean Warren <seanwarren77@gmail.com>
2024-05-23 11:33:50 -0400

    generate elixir app with phx.new
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>
    Co-authored-by: Shaun Ellis <sdellis@users.noreply.github.com>
    Co-authored-by: Trey Pendragon <tpendragon@users.noreply.github.com>

---
+++ test/dpul_collections_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule DpulCollectionsWeb.ErrorJSONTest do
+  use DpulCollectionsWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert DpulCollectionsWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert DpulCollectionsWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
