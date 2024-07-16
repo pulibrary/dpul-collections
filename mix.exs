fa5c12feffa37a87ee173bb6632fef98068244c8
Trey Pendragon <tpendragon@princeton.edu>
2024-07-16 14:55:06 -0700

    Add test coverage measuring
    This adds excoveralls (which is the state of the art coverage tool) and
    a `mix spec` command which will run the whole suite with an HTML export
    of coverage.
    
    Closes #37

--- mix.exs
+++ mix.exs
@@ -9,7 +9,16 @@ defmodule DpulCollections.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test,
+        spec: :test
+      ]
     ]
   end
 
@@ -57,7 +66,8 @@ defmodule DpulCollections.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:excoveralls, "~> 0.18", only: :test}
     ]
   end
 
@@ -79,7 +89,8 @@ defmodule DpulCollections.MixProject do
         "tailwind dpul_collections --minify",
         "esbuild dpul_collections --minify",
         "phx.digest"
-      ]
+      ],
+      spec: ["coveralls.html"]
     ]
   end
 end
