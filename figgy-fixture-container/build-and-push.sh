5b5337b7cd1d5c7006728d7c133c5d6a659176b7
Trey Pendragon <tpendragon@princeton.edu>
2024-07-18 14:37:41 -0700

    Improve variable name.

--- figgy-fixture-container/build-and-push.sh
+++ figgy-fixture-container/build-and-push.sh
@@ -4,7 +4,7 @@
 echo "Enter lastpass username:" 
 read USERNAME
 lpass login $USERNAME
-export CR_PAT=$(lpass show "Shared-ITIMS-Passwords/dpul_collections_fixture_container_github_token" --notes)
-echo $CR_PAT | docker login ghcr.io -u pulbot --password-stdin
+export GHCR_TOKEN=$(lpass show "Shared-ITIMS-Passwords/dpul_collections_fixture_container_github_token" --notes)
+echo $GHCR_TOKEN | docker login ghcr.io -u pulbot --password-stdin
 docker buildx create --name multiarch --driver docker-container --use || true
 docker buildx build --push --platform linux/arm64,linux/amd64 -t ghcr.io/pulibrary/dpul-collections:figgy-fixtures .
