49bbb11206215cf06ee3b6f8c6d70e26b78b618f
Trey Pendragon <tpendragon@princeton.edu>
2024-07-19 14:11:46 -0700

    Add formatting necessary.

--- test/dpul_collections_web/controllers/error_html_test.exs
+++ test/dpul_collections_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule DpulCollectionsWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(DpulCollectionsWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(DpulCollectionsWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
--- test/dpul_collections_web/controllers/error_json_test.exs
+++ test/dpul_collections_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule DpulCollectionsWeb.ErrorJSONTest do
   use DpulCollectionsWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert DpulCollectionsWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert DpulCollectionsWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
