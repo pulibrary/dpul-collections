696f73010d81b1da2181a56c6423bc3de22137ed
Trey Pendragon <tpendragon@princeton.edu>
2024-07-19 08:48:36 -0700

    Remove uncovered generated code, or ignore framework code.
    This gets 100% coverage.

--- lib/dpul_collections_web/router.ex
+++ lib/dpul_collections_web/router.ex
@@ -10,10 +10,6 @@ defmodule DpulCollectionsWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", DpulCollectionsWeb do
     pipe_through :browser
 
