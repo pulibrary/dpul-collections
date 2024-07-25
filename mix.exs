77953985db51e39bc4d8554c2da7998a9794054b
Eliot Jordan <eliotj@princeton.edu>
2024-07-25 14:40:47 -0500

    Add broadway package

--- mix.exs
+++ mix.exs
@@ -67,7 +67,8 @@ defmodule DpulCollections.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:excoveralls, "~> 0.18", only: :test}
+      {:excoveralls, "~> 0.18", only: :test},
+      {:broadway, "~> 1.0"}
     ]
   end
 
