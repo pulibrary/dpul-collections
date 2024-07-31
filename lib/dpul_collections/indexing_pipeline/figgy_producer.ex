2f95466f678a1411971de54359bcea9b371531b8
Anna Headley <845363+hackartisan@users.noreply.github.com>
2024-07-31 12:15:19 -0400

    WIP figgy producer handle demand with consecutive state
    Co-authored-by: Amin Zare <AminEdZare@users.noreply.github.com>
    Co-authored-by: Eliot Jordan <eliotjordan@users.noreply.github.com>
    Co-authored-by: Shaun Ellis <sdellis@users.noreply.github.com>

--- lib/dpul_collections/indexing_pipeline/figgy_producer.ex
+++ lib/dpul_collections/indexing_pipeline/figgy_producer.ex
@@ -10,13 +10,23 @@ defmodule DpulCollections.IndexingPipeline.FiggyProducer do
     {:producer, counter}
   end
 
-  def handle_demand(demand, state=%{last_queried_marker: nil, pulled_records: [], acked_records: []}) when demand > 0 do
+  def handle_demand(demand, state=%{last_queried_marker: nil}) when demand > 0 do
     records = IndexingPipeline.get_figgy_resources_since!(~U[1900-01-01 00:00:00Z], demand)
-    {:noreply, records, state}
+    new_state = %{
+      last_queried_marker: Enum.at(records, 0).updated_at,
+      pulled_records: [Enum.at(records, 0).id],
+      acked_records: []
+    }
+    {:noreply, records, new_state}
   end
 
-  def handle_demand(demand, state) when demand > 0 do
-    # records = IndexingPipeline.get_figgy_resources_since!(~U[1900-01-01 00:00:00Z], demand)
-    # {:noreply, records, state}
+  def handle_demand(demand, state=%{last_queried_marker: last_queried_marker, pulled_records: pulled_records, acked_records: acked_records}) when demand > 0 do
+    records = IndexingPipeline.get_figgy_resources_since!(last_queried_marker, demand)
+    new_state = %{
+      last_queried_marker: Enum.at(records, -1).updated_at,
+      pulled_records: pulled_records ++ Enum.map(records, fn r -> r.id end),
+      acked_records: acked_records
+    }
+    {:noreply, records, new_state}
   end
 end
--- test/dpul_collections/indexing_pipeline/figgy_producer_test.exs
+++ test/dpul_collections/indexing_pipeline/figgy_producer_test.exs
@@ -4,11 +4,44 @@ defmodule DpulCollections.IndexingPipeline.FiggyProducerTest do
   alias DpulCollections.IndexingPipeline.FiggyProducer
 
   describe "FiggyProducer" do
-    test "handle_demand/2 returns figgy resources" do
-      initial_state = %{last_queried_marker: nil, pulled_records: [], acked_records: []}
-      {:noreply, records, _state} = FiggyProducer.handle_demand(1, initial_state)
+    test "handle_demand/2 with initial state returns figgy resources" do
+      initial_state = %{last_queried_marker: nil}
+      {:noreply, records, new_state} = FiggyProducer.handle_demand(1, initial_state)
       assert Enum.at(records, 0).id == "3cb7627b-defc-401b-9959-42ebc4488f74"
+      IO.inspect(Enum.at(records, 0))
 
+      expected_state =
+        %{
+          last_queried_marker: Enum.at(records, 0).updated_at,
+          pulled_records: [Enum.at(records, 0).id],
+          acked_records: []
+        }
+      assert new_state == expected_state
+    end
+
+    test "handle_demand/2 with consecutive state and demand > 1 returns figgy resources" do
+      initial_state =
+        %{
+          last_queried_marker: ~U[2018-03-09 20:19:33Z],
+          pulled_records: ["3cb7627b-defc-401b-9959-42ebc4488f74"],
+          acked_records: []
+        }
+      {:noreply, records, new_state} = FiggyProducer.handle_demand(2, initial_state)
+      record1 = Enum.at(records, 0)
+      record2 = Enum.at(records, 1)
+      # we will get the same record again because we're doing >= on the
+      # last_queried_marker date stamp, to make sure
+      # we don't miss any records.
+      assert record1.id == "3cb7627b-defc-401b-9959-42ebc4488f74"
+      assert record2.id == "69990556-434c-476a-9043-bbf9a1bda5a4"
+
+      expected_state =
+        %{
+          last_queried_marker: Enum.at(records, -1).updated_at,
+          pulled_records: Enum.map(records, fn r -> r.id end),
+          acked_records: []
+        }
+      assert new_state == expected_state
     end
   end
 end
