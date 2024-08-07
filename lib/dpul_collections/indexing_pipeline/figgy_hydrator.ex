14c8c269463f16a9281bbc67ec2127a6f3822670
Trey Pendragon <tpendragon@princeton.edu>
2024-08-07 09:31:17 -0700

    Start the hydrator.
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>
    Co-authored-by: Eliot Jordan <eliotjordan@users.noreply.github.com>
    Co-authored-by: Shaun Ellis <sdellis@users.noreply.github.com>

---
+++ lib/dpul_collections/indexing_pipeline/figgy_hydrator.ex
@@ -0,0 +1,5 @@
+defmodule DpulCollections.IndexingPipeline.FiggyHydrator do
+  @moduledoc """
+  Broadway consumer that demands Figgy records and caches them in the database.
+  """
+end
---
+++ test/dpul_collections/indexing_pipeline/figgy_hydrator_test.exs
@@ -0,0 +1,12 @@
+defmodule DpulCollections.IndexingPipeline.FiggyHydratorTest do
+  use DpulCollections.DataCase
+
+  alias DpulCollections.IndexingPipeline.FiggyHydrator
+
+  describe "FiggyHydrator" do
+    # TODO: Look at https://hexdocs.pm/broadway/Broadway.html#module-testing
+    test "handle_message/3" do
+
+    end
+  end
+end
