c97341640bb06fcd9d1f0bff8616ed68024c24b7
Shaun Ellis <shaun@sdellis.com>
2024-08-07 13:55:01 -0400

    WIP: Hydrator Test Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com> Co-authored-by: Eliot Jordan <eliotjordan@users.noreply.github.com>

--- lib/dpul_collections/indexing_pipeline/figgy_hydrator.ex
+++ lib/dpul_collections/indexing_pipeline/figgy_hydrator.ex
@@ -2,4 +2,34 @@ defmodule DpulCollections.IndexingPipeline.FiggyHydrator do
   @moduledoc """
   Broadway consumer that demands Figgy records and caches them in the database.
   """
+  use Broadway
+
+  def start_link() do
+    producer_module = Application.fetch_env!(:dpul_collections, :producer_module)
+    producer_options = Application.get_env(:dpul_collections, :producer_options, [])
+
+    Broadway.start_link(__MODULE__,
+      name: __MODULE__,
+      producer: [
+        module: {producer_module, producer_options}
+      ],
+      processors: [
+        default: []
+      ],
+      batchers: [
+        default: [batch_size: 10]
+      ]
+    )
+  end
+
+  @impl true
+  def handle_message(_processor, message, _context) do
+    message
+  end
+
+  @impl true
+  def handle_batch(_batcher, messages, _batch_info, _context) do
+    messages
+  end
+
 end
--- test/dpul_collections/indexing_pipeline/figgy_hydrator_test.exs
+++ test/dpul_collections/indexing_pipeline/figgy_hydrator_test.exs
@@ -6,7 +6,8 @@ defmodule DpulCollections.IndexingPipeline.FiggyHydratorTest do
   describe "FiggyHydrator" do
     # TODO: Look at https://hexdocs.pm/broadway/Broadway.html#module-testing
     test "handle_message/3" do
-
+      ref = Broadway.test_message(FiggyHydrator, 1)
+      assert_receive {:ack, ^ref, [%{data: 1}], []}
     end
   end
 end
