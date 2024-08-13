8f533730fc2bbfabc3f8d5c7026d659ca9323912
Trey Pendragon <tpendragon@princeton.edu>
2024-08-13 08:31:35 -0700

    Add test for data.
    Co-authored-by: Amin Zare <AminEdZare@users.noreply.github.com>
    Co-authored-by: Eliot Jordan <eliotjordan@users.noreply.github.com>

--- test/dpul_collections/indexing_pipeline/figgy_hydrator_test.exs
+++ test/dpul_collections/indexing_pipeline/figgy_hydrator_test.exs
@@ -17,6 +17,7 @@ defmodule DpulCollections.IndexingPipeline.FiggyHydratorTest do
       assert cache_entry.record_id == "3cb7627b-defc-401b-9959-42ebc4488f74"
       assert cache_entry.cache_version == 0
       assert cache_entry.source_cache_order == ~U[2018-03-09 20:19:33.414040Z]
+      %{"id" => "3cb7627b-defc-401b-9959-42ebc4488f74", "internal_resource" => "EphemeraTerm"} = cache_entry.data
 
       # assert cache_entry.data == 1
       # %DpulCollections.IndexingPipeline.FiggyResource{__meta__: #Ecto.Schema.Metadata<:loaded, "orm_resources">, id: "3cb7627b-defc-401b-9959-42ebc4488f74", internal_resource: "EphemeraTerm", lock_version: nil, metadata: %{"code" => ["ja"], "edit_groups" => [], "edit_users" => [], "label" => ["Japan"], "lcsh_label" => [], "member_of_vocabulary_id" => [%{"id" => "22597862-062b-4c91-a6ca-cd73a30aceb1"}], "new_record" => false, "read_groups" => [], "read_users" => [], "tgm_label" => [], "uri" => []}, created_at: ~U[2018-02-22 19:13:49.949551Z], updated_at: ~U[2018-03-09 20:19:33.414040Z]}
