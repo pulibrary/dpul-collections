5aae11ba8fefaf3bf88dc4b73c18c966640107e8
Shaun Ellis <shaun@sdellis.com>
2024-09-17 14:59:31 -0400

    Started indexing consumer WIP
    Co-authored-by: Trey Pendragon <tpendragon@princeton.edu>
    Co-authored-by: Eliot Jordan <eliotj@princeton.edu>
    Co-authored-by: Anna Headley <aheadley@princeton.edu>

---
+++ lib/dpul_collections/indexing_pipeline/figgy/indexing_consumer.ex
@@ -0,0 +1,63 @@
+defmodule DpulCollections.IndexingPipeline.Figgy.IndexingConsumer do
+  @moduledoc """
+  Broadway consumer that demands Figgy.TransformationCacheEntry records and indexes
+  them into Solr.
+  """
+  alias DpulCollections.IndexingPipeline
+  alias DpulCollections.IndexingPipeline.Figgy
+  alias DpulCollections.Solr
+  use Broadway
+
+  @type start_opts ::
+          {:cache_version, Integer}
+          | {:producer_module, Module}
+          | {:producer_options, any()}
+          | {:batch_size, Integer}
+  @spec start_link([start_opts()]) :: Broadway.on_start()
+  def start_link(options \\ []) do
+    default = [
+      cache_version: 0,
+      producer_module: Figgy.IndexingProducer,
+      producer_options: 0,
+      batch_size: 10
+    ]
+
+    options = Keyword.merge(default, options)
+
+    Broadway.start_link(__MODULE__,
+      name: __MODULE__,
+      producer: [
+        module: {options[:producer_module], options[:producer_options]}
+      ],
+      processors: [
+        default: []
+      ],
+      batchers: [
+        default: [batch_size: options[:batch_size]]
+      ],
+      context: %{cache_version: options[:cache_version]}
+    )
+  end
+
+  @impl Broadway
+  # return the message so we can handle it in the batch
+  @spec handle_message(any(), Broadway.Message.t(), %{required(:cache_version) => integer()}) ::
+          Broadway.Message.t()
+  def handle_message(_processor, message, %{cache_version: _cache_version}) do
+    message
+  end
+
+  @impl Broadway
+  @spec handle_batch(any(), list(Broadway.Message.t()), any(), any()) ::
+          list(Broadway.Message.t())
+  def handle_batch(_batcher, messages, _batch_info, _context) do
+    messages 
+    |> Enum.map(&unwrap/1)
+    |> Solr.add()
+    messages
+  end
+
+  defp unwrap(%Broadway.Message{data: %Figgy.TransformationCacheEntry{data: data}}) do
+    data
+  end
+end
