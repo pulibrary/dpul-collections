849f5e33fb3fd762a96bcb562b396b602454edd0
Trey Pendragon <tpendragon@princeton.edu>
2024-09-19 09:21:59 -0700

    Add trap_exit to indexing producer.
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>
    Co-authored-by: Eliot Jordan <eliotjordan@users.noreply.github.com>
    Co-authored-by: Shaun Ellis <sdellis@users.noreply.github.com>

--- lib/dpul_collections/indexing_pipeline/figgy/indexing_producer.ex
+++ lib/dpul_collections/indexing_pipeline/figgy/indexing_producer.ex
@@ -22,6 +22,10 @@ defmodule DpulCollections.IndexingPipeline.Figgy.IndexingProducer do
         }
   @spec init(integer()) :: {:producer, state()}
   def init(cache_version) do
+    # trap the exit so we can stop gracefully
+    # see https://www.erlang.org/doc/apps/erts/erlang.html#process_flag/2
+    Process.flag(:trap_exit, true)
+
     last_queried_marker =
       IndexingPipeline.get_processor_marker!("figgy_indexer", cache_version)
 
@@ -125,6 +129,13 @@ defmodule DpulCollections.IndexingPipeline.Figgy.IndexingProducer do
 
     notify_ack(pending_markers |> length())
     {:noreply, messages, new_state}
+  end
+
+  # When we stop broadway we don't want to allow any more polling because
+  # the asynchronous :check_for_updates messages cause db connection errors
+  def handle_info({:EXIT, _PID, _reason}, state) do
+    state = Map.put(state, :stored_demand, 0)
+    {:stop, [], state}
   end
 
   # Updates state, removing any acked_records from pulled_records and returns the
