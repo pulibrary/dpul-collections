59481e07076e1ee8dc57ac24398abfa61dd85e4a
Eliot Jordan <eliotj@princeton.edu>
2024-09-24 15:44:15 -0500

    Add figgy database config to runtime.exs Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com> Co-authored-by: Shaun Ellis <sdellis@users.noreply.github.com> Co-authored-by: Trey Pendragon <tpendragon@users.noreply.github.com>

--- config/runtime.exs
+++ config/runtime.exs
@@ -36,6 +36,19 @@ if config_env() == :prod do
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
 
+  figgy_database_url =
+    System.get_env("FIGGY_DATABASE_URL") ||
+      raise """
+      environment variable FIGGY_DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
+
+  # Configure your other database
+  config :dpul_collections, DpulCollections.FiggyRepo,
+    url: figgy_database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    socket_options: maybe_ipv6
+
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
