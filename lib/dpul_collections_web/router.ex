f709c32aabc225ecb8e9827a81e5c45145098226
Eliot Jordan <eliotj@princeton.edu>
2024-09-25 15:57:39 -0500

    Try broadway dashboard

--- lib/dpul_collections_web/router.ex
+++ lib/dpul_collections_web/router.ex
@@ -1,5 +1,6 @@
 defmodule DpulCollectionsWeb.Router do
   use DpulCollectionsWeb, :router
+  import Phoenix.LiveDashboard.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -14,6 +15,10 @@ defmodule DpulCollectionsWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    live_dashboard "/dashboard", metrics: DpulCollectionsWeb.Telemetry,
+      additional_pages: [
+        broadway: BroadwayDashboard
+      ]
   end
 
   # Other scopes may use custom stacks.
@@ -22,19 +27,19 @@ defmodule DpulCollectionsWeb.Router do
   # end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:dpul_collections, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: DpulCollectionsWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
-  end
+  # if Application.compile_env(:dpul_collections, :dev_routes) do
+  #   # If you want to use the LiveDashboard in production, you should put
+  #   # it behind authentication and allow only admins to access it.
+  #   # If your application does not have an admins-only section yet,
+  #   # you can use Plug.BasicAuth to set up some basic authentication
+  #   # as long as you are also using SSL (which you should anyway).
+  #   import Phoenix.LiveDashboard.Router
+  #
+  #   scope "/dev" do
+  #     pipe_through :browser
+  #
+  #     live_dashboard "/dashboard", metrics: DpulCollectionsWeb.Telemetry
+  #     forward "/mailbox", Plug.Swoosh.MailboxPreview
+  #   end
+  # end
 end
