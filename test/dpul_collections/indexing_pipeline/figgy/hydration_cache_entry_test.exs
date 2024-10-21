8ba296190cb1a2b837e25801e1ada4c8b7089806
Trey Pendragon <tpendragon@princeton.edu>
2024-10-21 10:23:34 -0700

    Index page count.
    Closes #121

--- test/dpul_collections/indexing_pipeline/figgy/hydration_cache_entry_test.exs
+++ test/dpul_collections/indexing_pipeline/figgy/hydration_cache_entry_test.exs
@@ -17,6 +17,17 @@ defmodule DpulCollections.IndexingPipeline.Figgy.HydrationCacheEntryTest do
       assert doc3[:description_txtm] == nil
     end
 
+    test "indexes page_count_i" do
+      entries =
+        FiggyTestFixtures.hydration_cache_entries()
+        |> Tuple.to_list()
+
+      [doc1, doc2, _doc3] = Enum.map(entries, &HydrationCacheEntry.to_solr_document/1)
+
+      assert doc1[:page_count_i] == 27
+      assert doc2[:page_count_i] == 0
+    end
+
     test "includes date range if found, date if not" do
       entries =
         FiggyTestFixtures.hydration_cache_entries()
--- test/support/fixtures/figgy_test_fixtures.ex
+++ test/support/fixtures/figgy_test_fixtures.ex
@@ -40,6 +40,35 @@ defmodule FiggyTestFixtures do
           "metadata" => %{
             "title" => ["test title 1"],
             "description" => ["Asra-Panahi", "Berlin-Protest", "Elnaz-Rekabi"],
+            "member_ids" => [
+              %{"id" => "f60ce0c9-57fc-4820-b70d-49d1f2b248f9"},
+              %{"id" => "d798d940-0740-4854-8f70-60217ec8c2e4"},
+              %{"id" => "31d69702-4d4e-4e07-a65c-a30f62d096fd"},
+              %{"id" => "899b015e-0e92-4b3d-a300-4a50fe641c11"},
+              %{"id" => "39f54327-7739-4184-90ca-5b78fac3ca64"},
+              %{"id" => "5e53e486-1bb8-45ee-bc35-adaa708f35ef"},
+              %{"id" => "d13ad936-bf2c-4a53-8bc1-a20bf6841b74"},
+              %{"id" => "86982ae7-3f95-4e77-aecb-14ad8e5710b7"},
+              %{"id" => "725a3fac-820b-4333-9147-99790de6d1fe"},
+              %{"id" => "ded111e0-26c2-4afc-89b8-4ad4ece3275a"},
+              %{"id" => "f58a6245-6b83-4cff-a61f-cc6aa675a0ef"},
+              %{"id" => "5437c9fe-e4c7-4f93-bf2c-ad9afee75bf5"},
+              %{"id" => "ce66d035-5dd5-4df1-b825-d02746f9c11a"},
+              %{"id" => "703c1ca1-2801-45fe-b2c5-607ec413f619"},
+              %{"id" => "87dd1c80-3c94-4580-9273-dd54173a99f0"},
+              %{"id" => "0eb9e3ae-fc5f-4902-8bbb-8706f081844e"},
+              %{"id" => "171268c2-9565-4418-b5f8-a52c11ac00ec"},
+              %{"id" => "6635328f-3b7e-4026-a0c5-4288e42254ed"},
+              %{"id" => "4b8da379-2618-4aa4-8632-58e57dca0803"},
+              %{"id" => "9887e5ab-2f82-4feb-be67-e2f726511c88"},
+              %{"id" => "b6c8c6c3-5f0e-4334-82d1-cc39da657a25"},
+              %{"id" => "9bbce8ac-a2e3-4200-8100-075273cf7d72"},
+              %{"id" => "660a8add-ba14-445b-8371-73fba8704eb3"},
+              %{"id" => "3f23a474-b08c-4b52-9ad8-930ff962a8cf"},
+              %{"id" => "9b036f96-5080-48dc-9f32-cf9659bb0764"},
+              %{"id" => "b38427c2-6126-41ce-8624-b1c435378f0c"},
+              %{"id" => "994ee133-c117-40c2-89b5-0f6b7a705559"}
+            ],
             "date_created" => ["2022"],
             "date_range" => [
               %{
@@ -58,8 +87,6 @@ defmodule FiggyTestFixtures do
         }
       })
 
-    :timer.sleep(1)
-
     # date range data taken from 4c8cf820-69f1-4b0e-bf76-41b339af7c50
     {:ok, entry2} =
       IndexingPipeline.write_hydration_cache_entry(%{
@@ -73,6 +100,7 @@ defmodule FiggyTestFixtures do
             "title" => ["test title 2"],
             "description" => [],
             "date_created" => [],
+            "member_ids" => [],
             "date_range" => [
               %{
                 "approximate" => nil,
@@ -88,8 +116,6 @@ defmodule FiggyTestFixtures do
           }
         }
       })
-
-    :timer.sleep(1)
 
     {:ok, entry3} =
       IndexingPipeline.write_hydration_cache_entry(%{
