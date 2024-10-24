5a28f90d6d1664e4127248d0c69292f46f4bf6e6
Eliot Jordan <eliotj@princeton.edu>
2024-10-24 17:16:28 -0500

    Improve pagination behavior on search page

--- lib/dpul_collections_web/live/helpers.ex
+++ lib/dpul_collections_web/live/helpers.ex
@@ -1,9 +1,11 @@
 defmodule DpulCollectionsWeb.Live.Helpers do
-  # Remove KV pairs with nil or empty string values
-  def clean_params(params) do
+  # Remove KV pairs with nil or empty string values 
+  # or with Keys in a remove_keys list
+  def clean_params(params, remove_keys \\ []) do
     params
     |> Enum.reject(fn {_, v} -> v == "" end)
     |> Enum.reject(fn {_, v} -> is_nil(v) end)
+    |> Enum.reject(fn {k, _} -> Enum.member?(remove_keys, k) end)
     |> Enum.into(%{})
   end
 end
