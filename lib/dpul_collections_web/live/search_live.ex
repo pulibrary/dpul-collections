e534be57438f0bb677cf74621d4eaa9216adf960
Shaun Ellis <shaun@sdellis.com>
2024-11-04 17:04:14 -0500

    adds loader and page transition

--- lib/dpul_collections_web/live/search_live.ex
+++ lib/dpul_collections_web/live/search_live.ex
@@ -28,6 +28,23 @@ defmodule DpulCollectionsWeb.SearchLive do
   end
 
   def handle_params(params, _uri, socket) do
+    send(self(), {:run_search, params})
+    search_state = SearchState.from_params(params)
+
+    socket =
+      assign(socket,
+        search_state: search_state,
+        item_counter: 0,
+        items: [],
+        total_items: 0,
+        loading: true
+      )
+
+    {:noreply, socket}
+  end
+
+  def handle_info({:run_search, params}, socket) do 
+    # Process.sleep(1000)
     search_state = SearchState.from_params(params)
     solr_response = Solr.query(search_state)
 
@@ -42,7 +59,8 @@ defmodule DpulCollectionsWeb.SearchLive do
         search_state: search_state,
         item_counter: item_counter(search_state, total_items),
         items: items,
-        total_items: total_items
+        total_items: total_items,
+        loading: false
       )
 
     {:noreply, socket}
@@ -111,17 +129,24 @@ defmodule DpulCollectionsWeb.SearchLive do
       </div>
       <div id="item-counter">
         <span><%= @item_counter %></span>
+        <div class="text-center bg-white max-w-5xl mx-auto text-lg py-8">
+          <.paginator
+            page={@search_state.page}
+            per_page={@search_state.per_page}
+            total_items={@total_items}
+          />
+        </div>
       </div>
     </div>
-    <div class="grid grid-flow-row auto-rows-max gap-8">
+    <div id="search-results" class="grid grid-flow-row auto-rows-max gap-8">
+      <div :if={@loading} id="loader" class="w-100% text-center" role="status">
+          <svg aria-hidden="true" class="w-16 h-16 inline text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+          <span class="sr-only">Loading...</span>
+      </div>
       <.search_item :for={item <- @items} item={item} />
-    </div>
-    <div class="text-center bg-white max-w-5xl mx-auto text-lg py-8">
-      <.paginator
-        page={@search_state.page}
-        per_page={@search_state.per_page}
-        total_items={@total_items}
-      />
     </div>
     """
   end
@@ -131,7 +156,7 @@ defmodule DpulCollectionsWeb.SearchLive do
   def search_item(assigns) do
     ~H"""
     <hr />
-    <div class="item">
+    <div id={@item.id} class="item" phx-hook="Fade" >
       <div class="flex flex-wrap gap-5 md:max-h-60 max-h-[20rem] overflow-hidden justify-center md:justify-start relative">
         <.thumbs :for={_thumb <- 1..@item.page_count} :if={@item.page_count} />
         <div :if={@item.page_count > 1} class="absolute right-0 top-0 bg-white px-4 py-2">
