837735fc2af1e02b4682a63e984644d32f02227e
Anna Headley <845363+hackartisan@users.noreply.github.com>
2024-11-19 17:13:18 -0500

    Split up index_parity logic for clarity

--- lib/dpul_collections/indexing_pipeline/coherence.ex
+++ lib/dpul_collections/indexing_pipeline/coherence.ex
@@ -4,24 +4,10 @@ defmodule DpulCollections.IndexingPipeline.Coherence do
 
   @spec index_parity?() :: boolean()
   def index_parity?() do
-    pms =
+    index_progress_summaries =
       Application.fetch_env!(:dpul_collections, DpulCollections.IndexingPipeline)
-      |> Enum.map(fn pipeline ->
-        IndexingPipeline.get_processor_marker!("figgy_indexer", pipeline[:cache_version])
-      end)
-
-    # the cache_location in a processor marker isn't consistent between
-    # cache versions -- it's just a timestamp. So we pull the hydration
-    # cache entries and compare them based on the figgy timestamp itself
-    hydration_entries =
-      pms
-      |> Enum.map(fn marker ->
-        IndexingPipeline.get_hydration_cache_entry!(marker.cache_record_id, marker.cache_version)
-      end)
-
-    version_sorted = Enum.sort_by(hydration_entries, & &1.cache_version)
-    date_sorted = Enum.sort_by(hydration_entries, & &1.source_cache_order, DateTime)
-    version_sorted == date_sorted
+      |> Enum.map(&index_progress_summary/1)
+      |> is_new_cache_caught_up?()
   end
 
   @spec document_count_report() :: map()
@@ -34,5 +20,35 @@ defmodule DpulCollections.IndexingPipeline.Coherence do
         document_count: Solr.document_count(pipeline[:write_collection])
       }
     end)
+  end
+
+  # Check the processor marker for the most recently indexed record.
+  # Get the figgy timestamp for that record from its hydration cache entry.
+  defp index_progress_summary(cache_version: cache_version, write_collection: _) do
+    marker = IndexingPipeline.get_processor_marker!("figgy_indexer", cache_version)
+
+    hydration_entry =
+      IndexingPipeline.get_hydration_cache_entry!(
+        marker.cache_record_id,
+        cache_version
+      )
+
+    %{figgy_timestamp: hydration_entry.source_cache_order, cache_version: cache_version}
+  end
+
+  # if both indexes have hit the same figgy timestamp, we're caught up
+  defp is_new_cache_caught_up?([
+         %{figgy_timestamp: timestamps_are_equal},
+         %{figgy_timestamp: timestamps_are_equal}
+       ]) do
+    true
+  end
+
+  # otherwise, if the more recent index has passed the older index, we're caught
+  # up
+  defp is_new_cache_caught_up?(index_progress_summaries) do
+    version_sorted = Enum.sort_by(index_progress_summaries, & &1.cache_version)
+    date_sorted = Enum.sort_by(index_progress_summaries, & &1.figgy_timestamp, DateTime)
+    version_sorted == date_sorted
   end
 end
