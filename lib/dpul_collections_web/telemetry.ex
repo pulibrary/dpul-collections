be8b41ae133e7a8a82d2e25c8ee3c0c641e14ec9
Trey Pendragon <tpendragon@princeton.edu>
2024-11-27 10:30:58 -0800

    Format.

--- lib/dpul_collections_web/telemetry.ex
+++ lib/dpul_collections_web/telemetry.ex
@@ -78,7 +78,7 @@ defmodule DpulCollectionsWeb.Telemetry do
       summary("vm.memory.total", unit: {:byte, :kilobyte}),
       summary("vm.total_run_queue_lengths.total"),
       summary("vm.total_run_queue_lengths.cpu"),
-      summary("vm.total_run_queue_lengths.io"),
+      summary("vm.total_run_queue_lengths.io")
     ]
   end
 
