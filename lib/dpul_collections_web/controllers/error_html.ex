392a48f418e9f18f30ba9a0b2b3e03cf9b97c182
Anna Headley <845363+hackartisan@users.noreply.github.com>
2025-01-09 16:40:11 -0500

    Add non-live view for 404s

--- lib/dpul_collections_web/controllers/error_html.ex
+++ lib/dpul_collections_web/controllers/error_html.ex
@@ -7,13 +7,13 @@ defmodule DpulCollectionsWeb.ErrorHTML do
   use DpulCollectionsWeb, :html
 
   # If you want to customize your error pages,
-  # uncomment the embed_templates/1 call below
-  # and add pages to the error directory:
+  # add pages to the error directory, e.g.:
   #
   #   * lib/dpul_collections_web/controllers/error_html/404.html.heex
   #   * lib/dpul_collections_web/controllers/error_html/500.html.heex
   #
-  # embed_templates "error_html/*"
+  # they are used via the embed_templates/1 call below
+  embed_templates "error_html/*"
 
   # The default is to render a plain text page based on
   # the template name. For example, "404.html" becomes
