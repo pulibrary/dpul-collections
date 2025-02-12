6a844183f904af4b9314c4ea98b31e17877f3aae
Oh, Hey! <shaun@sdellis.com>
2025-02-12 15:08:43 -0500

    Adds initial markup and styling to the header (WIP)
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- lib/dpul_collections_web/components_live/header_component.ex
+++ lib/dpul_collections_web/components_live/header_component.ex
@@ -3,8 +3,10 @@ defmodule DpulCollectionsWeb.HeaderComponent do
 
     def render(assigns) do 
         ~H"""
-        <header>
-        hello world!
+        <header class="flex flex-row gap-10 bg-violet-100 py-10 px-8 lg:px-10">
+            <div class="logo flex-none">Logo</div>
+            <div class="app_name flex-1">DPUL Collections</div>
+            <div class="menu flex-none">Menu</div>
         </header>
         """
     end
