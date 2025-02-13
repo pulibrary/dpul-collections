d672ee65f992bcaabbc66a934b6c17c34d70e05b
Oh, Hey! <shaun@sdellis.com>
2025-02-13 16:05:24 -0500

    creates dropdown menu in the HeaderComponent

--- lib/dpul_collections_web/components_live/header_component.ex
+++ lib/dpul_collections_web/components_live/header_component.ex
@@ -1,12 +1,33 @@
 defmodule DpulCollectionsWeb.HeaderComponent do 
     use DpulCollectionsWeb, :live_component 
+    alias Phoenix.LiveView.JS 
 
     def render(assigns) do 
         ~H"""
         <header class="flex flex-row gap-10 bg-violet-100 py-10 px-8 lg:px-10">
             <div class="logo flex-none">Logo</div>
             <div class="app_name flex-1">DPUL Collections</div>
-            <div class="menu flex-none">Menu</div>
+            <div class="menu flex-none">
+                <div class="dropdown relative inline-block">
+                    <button id="dropdownButton" 
+                        aria-haspopup="true" 
+                        aria-expanded="false"
+                        phx-click={JS.toggle(to: "#dropdownMenu")}>
+                        Language
+                    </button>
+                    <ul id="dropdownMenu" class="dropdown-menu aria-hidden hidden absolute list-none bg-white w-150 p-0 m-0" role="menu" aria-hidden="true">
+                        <li role="menuitem" tabindex="-1" class="p-2 hover:bg-stone-200 focus:bg-stone-200">
+                            <a href="#">English</a>
+                        </li>
+                        <li role="menuitem" tabindex="-1" class="p-2 hover:bg-stone-200 focus:bg-stone-200">
+                            <a href="#">Español</a>
+                        </li>
+                        <li role="menuitem" tabindex="-1" class="p-2 hover:bg-stone-200 focus:bg-stone-200">
+                            <a href="#">Português do Brasil</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </header>
         """
     end
--- test/dpul_collections_web/components_live/header_component_test.exs
+++ test/dpul_collections_web/components_live/header_component_test.exs
@@ -6,7 +6,32 @@ defmodule DpulCollectionsWeb.HeaderComponentTest do
 
   test "HeaderComponent" do
     assert render_component(DpulCollectionsWeb.HeaderComponent, id: 123) =~
-         "<header>"
+         "<header"
   end
+
+  test "has a dropdown menu for language preference" do
+    assert render_component(DpulCollectionsWeb.HeaderComponent, id: 123) =~
+         "<button id=\"dropdownButton\""
+  end
+
+  # The test below returns the following error: 
+  # -- no push or navigation command found within JS commands: [["toggle",{"to":"#dropdownMenu"}]]
+  # The test environment does not execute frontend JS behaviors, 
+  # so the way to test this is to modify the component to use 
+  # phx-click with LiveView state changes (assigns) that can be tested
+
+  # test "clicking the button toggles dropdown visibility", %{conn: conn} do
+  #   {:ok, view, _html} = live(conn, ~p"/")
+
+  #   assert has_element?(view, "#dropdownMenu[aria-hidden='true']")
+
+  #   view |> element("#dropdownButton") |> render_click()
+
+  #   assert has_element?(view, "#dropdownMenu[aria-hidden='false']")
+
+  #   view |> element("#dropdownButton") |> render_click()
+
+  #   assert has_element?(view, "#dropdownMenu[aria-hidden='true']")
+  # end
 
 end
