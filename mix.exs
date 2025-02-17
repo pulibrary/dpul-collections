f4f62fcadf321763fe4ad9542d08bf2f8f7b6b31
Anna Headley <845363+hackartisan@users.noreply.github.com>
2025-02-17 12:26:57 -0500

    Looked into live_isolated_compoment
    see: https://github.com/Serabe/live_isolated_component
    previous assessment looks right to me, that we'd have to change the
    toggle via state (this allows us to test handle_event and handle_info).
    
    but given that LiveView.JS seems the right way to do a toggle, I don't
    think we want to go this direction

--- mix.exs
+++ mix.exs
@@ -50,6 +50,8 @@ defmodule DpulCollections.MixProject do
       {:phoenix_html, "~> 4.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.2"},
+      # live_isolated_component version depends on version of live view
+      {:live_isolated_component, "~> 0.8.0", only: [:dev, :test]},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
