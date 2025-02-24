3a6374940dcced983755e87a248c7acaa0723ef0
Oh, Hey! <shaun@sdellis.com>
2025-02-24 16:03:34 -0500

    fixes formatting

--- lib/dpul_collections_web/components/footer_component.ex
+++ lib/dpul_collections_web/components/footer_component.ex
@@ -1,35 +1,37 @@
 # lib/my_app_web/components/header_component.ex
 defmodule DpulCollectionsWeb.FooterComponent do
-    use DpulCollectionsWeb, :html
-    import DpulCollectionsWeb.Gettext
-  
-    def footer(assigns) do
-      ~H"""
-      <footer class="text-white flex flex-wrap sm:flex-row gap-10 items-center bg-gray-700 px-6 py-6 sm:py-10 sm:px-8 lg:px-10">
-        <div class="footer-links flex-none w-32 sm:w-60">
-            <ul class="text-xs">
-                <li class="py-1">
-                    <a href="https://library.princeton.edu/about/policies/copyright-and-permissions-policies">
-                         <%= gettext("Copyright Policy") %>
-                    </a></li>
-                <li class="py-1">
-                    <a href="https://www.princeton.edu/privacy-notice">
-                        <%= gettext("Privacy Notice") %>
-                    </a></li>
-                <li class="py-1">
-                    <a href="https://accessibility.princeton.edu/help">
-                        <%= gettext("Accessibility Help") %>
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div class="app_name sm:flex-1 flex-none text-center">
-            © 2025 <%= gettext("The Trustees of Princeton University") %> 
-        </div>
-        <div class="university-logo flex-none w-24 sm:w-48">
-            <img src={~p"/images/university-logo.svg"} alt="Princeton University Logo" />
-        </div>
-      </footer>
-      """
-    end
+  use DpulCollectionsWeb, :html
+  import DpulCollectionsWeb.Gettext
+
+  def footer(assigns) do
+    ~H"""
+    <footer class="text-white flex flex-wrap sm:flex-row gap-10 items-center bg-gray-700 px-6 py-6 sm:py-10 sm:px-8 lg:px-10">
+      <div class="footer-links flex-none w-32 sm:w-60">
+        <ul class="text-xs">
+          <li class="py-1">
+            <a href="https://library.princeton.edu/about/policies/copyright-and-permissions-policies">
+              <%= gettext("Copyright Policy") %>
+            </a>
+          </li>
+          <li class="py-1">
+            <a href="https://www.princeton.edu/privacy-notice">
+              <%= gettext("Privacy Notice") %>
+            </a>
+          </li>
+          <li class="py-1">
+            <a href="https://accessibility.princeton.edu/help">
+              <%= gettext("Accessibility Help") %>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="app_name sm:flex-1 flex-none text-center">
+        © 2025 <%= gettext("The Trustees of Princeton University") %>
+      </div>
+      <div class="university-logo flex-none w-24 sm:w-48">
+        <img src={~p"/images/university-logo.svg"} alt="Princeton University Logo" />
+      </div>
+    </footer>
+    """
   end
+end
