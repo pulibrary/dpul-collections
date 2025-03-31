1f772019fb6dd72f93f9bab8a9ace9bd9679bbe6
Trey Pendragon <tpendragon@princeton.edu>
2025-03-31 15:36:25 -0700

    Combine all the functions into one.

--- lib/dpul_collections/prom_ex/plugins/indexing_pipeline.ex
+++ lib/dpul_collections/prom_ex/plugins/indexing_pipeline.ex
@@ -1,35 +1,54 @@
 defmodule DpulCollections.PromEx.Plugins.IndexingPipeline do
   use PromEx.Plugin
+  @query_event [:dpul_collections, :indexing_pipeline, :query]
+  @query_functions [
+    :"get_figgy_resource!/1",
+    :"get_figgy_parents/1",
+    :"get_figgy_resources/2",
+    :"get_figgy_resources_since!/2"
+  ]
 
   @impl true
   def event_metrics(opts) do
+    set_up_telemetry_proxy()
+
     Event.build(
       :indexing_pipeline_metrics,
-      [
-        :"get_figgy_resource!/1",
-        :"get_figgy_parents/1",
-        :"get_figgy_resources/2",
-        :"get_figgy_resources_since!/2"
-      ]
-      |> Enum.map(fn func_name ->
-        distribution(
-          [
-            :dpul_collections,
-            :indexing_pipeline,
-            :query,
-            :duration,
-            :milliseconds
-          ],
-          event_name: [:dpul_collections, :indexing_pipeline, func_name, :stop],
-          measurement: :duration,
-          description: "Time for #{func_name} to return",
-          reporter_options: [
-            buckets: [10, 50, 250, 2_500, 10_000, 30_000]
-          ],
-          tags: [:function],
-          unit: {:native, :millisecond}
-        )
-      end)
+      distribution(
+        [
+          :dpul_collections,
+          :indexing_pipeline,
+          :query,
+          :duration,
+          :milliseconds
+        ],
+        event_name: @query_event,
+        measurement: :duration,
+        description: "Time for query to return",
+        reporter_options: [
+          buckets: [10, 50, 250, 2_500, 10_000, 30_000]
+        ],
+        tags: [:function],
+        unit: {:native, :millisecond}
+      )
     )
+  end
+
+  def handle_proxy_query_event(_event_name, event_measurement, event_metadata, _config) do
+    :telemetry.execute(@query_event, event_measurement, event_metadata)
+  end
+
+  defp set_up_telemetry_proxy() do
+    @query_functions
+    |> Enum.each(fn func_name ->
+      query_event = [:dpul_collections, :indexing_pipeline, func_name, :stop]
+
+      :telemetry.attach(
+        [:prom_ex, :indexing_pipeline, :proxy] ++ func_name,
+        query_event,
+        &__MODULE__.handle_proxy_query_event/4,
+        %{}
+      )
+    end)
   end
 end
