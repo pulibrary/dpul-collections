d65e4b338f092e90ded6a897b465d7b7153879ff
Trey Pendragon <tpendragon@princeton.edu>
2025-04-01 19:09:48 -0700

    Add plug-cowboy.

--- mix.exs
+++ mix.exs
@@ -79,7 +79,8 @@ defmodule DpulCollections.MixProject do
       {:live_debugger, "~> 0.1.4", only: :dev},
       {:prom_ex, "~> 1.11.0"},
       {:ecto_psql_extras, "~> 0.6"},
-      {:sibyl, "~> 0.1.0"}
+      {:sibyl, "~> 0.1.0"},
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 
