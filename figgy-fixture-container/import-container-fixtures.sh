9cce263207ab4c7fd2647bd16146b30067b4a191
Eliot Jordan <eliotj@princeton.edu>
2025-04-18 13:56:45 -0500

    Use stock Postgres containers for Figgy fixtures
    Removes dependence on pre-built fixture images and instead loads fixture
    records into a Postgres container from the command line.

--- figgy-fixture-container/import-container-fixtures.sh
+++ figgy-fixture-container/import-container-fixtures.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-pg_restore -d "$POSTGRES_DB" --no-owner --no-privileges --role=$POSTGRES_USER /tmp/fixture-exports/db-schema.sql
+# Setup host and port variables for local development and CI.
+if [ -n "$TEST_POSTGRES_FIGGY_HOST" ]; then
+  export PG_FIGGY_HOST=$TEST_POSTGRES_FIGGY_HOST
+else
+  export PG_FIGGY_HOST=localhost
+fi
 
-psql -d "$POSTGRES_DB" -U $POSTGRES_USER -c "\copy orm_resources FROM /tmp/fixture-exports/project-export.binary BINARY"
-psql -d "$POSTGRES_DB" -U $POSTGRES_USER -c "\copy orm_resources FROM /tmp/fixture-exports/project-members-export.binary BINARY"
-psql -d "$POSTGRES_DB" -U $POSTGRES_USER -c "\copy orm_resources FROM /tmp/fixture-exports/project-vocabulary-export.binary BINARY"
-psql -d "$POSTGRES_DB" -U $POSTGRES_USER -c "\copy orm_resources FROM /tmp/fixture-exports/project-dm-export.binary BINARY"
-psql -d "$POSTGRES_DB" -U $POSTGRES_USER -c "\copy orm_resources FROM /tmp/fixture-exports/project-ignore-export.binary BINARY"
-psql -d "$POSTGRES_DB" -U $POSTGRES_USER -c "\copy orm_resources FROM /tmp/fixture-exports/project-ignore2-export.binary BINARY"
-psql -d "$POSTGRES_DB" -U $POSTGRES_USER -c "\copy orm_resources FROM /tmp/fixture-exports/extras-export.binary BINARY"
-psql -d "$POSTGRES_DB" -U $POSTGRES_USER -c "\copy orm_resources FROM /tmp/fixture-exports/extras-members-export.binary BINARY"
+if [ -n "$TEST_POSTGRES_FIGGY_PORT" ]; then
+  export PG_FIGGY_PORT=$TEST_POSTGRES_FIGGY_PORT
+else
+  export PG_FIGGY_PORT=5435
+fi
 
-for file in /tmp/fixture-exports/synthetic/*.csv; do
+pg_restore -h $PG_FIGGY_HOST -p $PG_FIGGY_PORT -U postgres -d "postgres" --no-owner --no-privileges --role=postgres fixture-exports/db-schema.sql
+
+psql -h $PG_FIGGY_HOST -p $PG_FIGGY_PORT -U postgres -c "\copy orm_resources FROM fixture-exports/project-export.binary BINARY"
+psql -h $PG_FIGGY_HOST -p $PG_FIGGY_PORT -U postgres -c "\copy orm_resources FROM fixture-exports/project-members-export.binary BINARY"
+psql -h $PG_FIGGY_HOST -p $PG_FIGGY_PORT -U postgres -c "\copy orm_resources FROM fixture-exports/project-vocabulary-export.binary BINARY"
+psql -h $PG_FIGGY_HOST -p $PG_FIGGY_PORT -U postgres -c "\copy orm_resources FROM fixture-exports/project-dm-export.binary BINARY"
+psql -h $PG_FIGGY_HOST -p $PG_FIGGY_PORT -U postgres -c "\copy orm_resources FROM fixture-exports/project-ignore-export.binary BINARY"
+psql -h $PG_FIGGY_HOST -p $PG_FIGGY_PORT -U postgres -c "\copy orm_resources FROM fixture-exports/project-ignore2-export.binary BINARY"
+psql -h $PG_FIGGY_HOST -p $PG_FIGGY_PORT -U postgres -c "\copy orm_resources FROM fixture-exports/extras-export.binary BINARY"
+psql -h $PG_FIGGY_HOST -p $PG_FIGGY_PORT -U postgres -c "\copy orm_resources FROM fixture-exports/extras-members-export.binary BINARY"
+
+# Import synthetic fixtures
+for file in fixture-exports/synthetic/*.csv; do
   # Check if it's a file (not a directory)
   if [ -f "$file" ]; then
-    psql -d "$POSTGRES_DB" -U $POSTGRES_USER -c "\copy orm_resources FROM $file CSV"
+    psql -h $PG_FIGGY_HOST -p $PG_FIGGY_PORT -U postgres -c "\copy orm_resources FROM $file CSV"
   fi
 done
