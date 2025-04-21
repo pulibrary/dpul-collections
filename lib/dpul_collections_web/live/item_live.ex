895ba067dd12b5bb344e93e432c402ae2b8b58e8
Anna Headley <845363+hackartisan@users.noreply.github.com>
2025-04-21 16:57:24 -0400

    Get general layout in place

--- lib/dpul_collections_web/live/item_live.ex
+++ lib/dpul_collections_web/live/item_live.ex
@@ -27,46 +27,75 @@ defmodule DpulCollectionsWeb.ItemLive do
 
   def render(assigns) do
     ~H"""
-    <div class="my-5 grid grid-flow-row auto-rows-max md:grid-cols-5 gap-4">
-      <div class="item md:col-span-3 md:pl-8">
-        <h1 class="pb-2">{@item.title}</h1>
-        <div class="md:block hidden">
+    <div class="content-area">
+      <div class="column-layout my-5 flex flex-col sm:grid sm:grid-flow-row sm:auto-rows-0 sm:grid-cols-5 sm:grid-rows-[auto_1fr] sm:content-start gap-4">
+        <div class="item-title sm:row-start-1 sm:col-start-3 sm:col-span-3 sm:pl-8 h-min">
+          <p>{@item.genre}</p>
+          <h1 class="pb-2">{@item.title}</h1>
+          <p>{@item.transliterated_title}</p>
+          <p>{@item.alternative_title}</p>
+          <p>{@item.date}</p>
+        </div>
+
+        <div class="thumbnails w-[26rem] sm:row-start-1 sm:col-start-1 sm:col-span-2 sm:row-span-2">
+          <.primary_thumbnail item={@item} />
+
+          <div class="bg-cloud sm:hidden">action bar</div>
+
+          <section class="page-thumbnails hidden sm:block md:col-span-2 py-4">
+            <h2 class="py-4">{gettext("Pages")} ({@item.file_count})</h2>
+            <div class="flex flex-wrap gap-5 justify-center md:justify-start">
+              <.thumbs
+                :for={{thumb, thumb_num} <- Enum.with_index(@item.image_service_urls)}
+                :if={@item.file_count}
+                thumb={thumb}
+                thumb_num={thumb_num}
+              />
+            </div>
+          </section>
+        </div>
+
+        <div class="metadata sm:row-start-2 sm:col-span-3 sm:col-start-3">
+          <div class="bg-cloud">description & collection</div>
+          <div class="bg-cloud hidden sm:block">action bar</div>
           <.metadata_table item={@item} />
         </div>
       </div>
-      <div class="primary-thumbnail md:col-span-2 md:order-first">
-        <img
-          class="w-full"
-          src={"#{@item.primary_thumbnail_service_url}/full/525,800/0/default.jpg"}
-          alt="main image display"
-          style="
+
+      <div class="">
+        <div class="bg-cloud">RELATED ITEMS</div>
+      </div>
+    </div>
+    """
+  end
+
+  def primary_thumbnail(assigns) do
+    ~H"""
+    <div class="primary-thumbnail grid grid-cols-2 gap-2 content-start">
+      <img
+        class="col-span-2 w-full"
+        src={"#{@item.primary_thumbnail_service_url}/full/525,800/0/default.jpg"}
+        alt="main image display"
+        style="
           background-color: lightgray;"
-          width="525"
-          height="800"
-        />
-        <button class="w-full btn-primary">
-          <a
-            href={"#{Application.fetch_env!(:dpul_collections, :web_connections)[:figgy_url]}/catalog/#{@item.id}/pdf"}
-            target="_blank"
-          >
-            {gettext("Download PDF")}
-          </a>
-        </button>
-      </div>
-      <div class="md:hidden block">
-        <.metadata_table item={@item} />
-      </div>
-      <section class="md:col-span-5 m:order-last py-4">
-        <h2 class="py-4">{gettext("Pages")} ({@item.file_count})</h2>
-        <div class="flex flex-wrap gap-5 justify-center md:justify-start">
-          <.thumbs
-            :for={{thumb, thumb_num} <- Enum.with_index(@item.image_service_urls)}
-            :if={@item.file_count}
-            thumb={thumb}
-            thumb_num={thumb_num}
-          />
-        </div>
-      </section>
+        width="525"
+        height="800"
+      />
+
+      <button class="btn-primary">
+        <a href="#" target="_blank">
+          {gettext("View")}
+        </a>
+      </button>
+
+      <button class="btn-primary">
+        <a
+          href={"#{Application.fetch_env!(:dpul_collections, :web_connections)[:figgy_url]}/catalog/#{@item.id}/pdf"}
+          target="_blank"
+        >
+          {gettext("Download PDF")}
+        </a>
+      </button>
     </div>
     """
   end
