3b39b54b44f417ec353a898a940a8369797a5a1f
Oh, Hey! <shaun@sdellis.com>
2025-04-30 14:54:15 -0400

    mix format

--- test/support/test_utils.ex
+++ test/support/test_utils.ex
@@ -1,11 +1,11 @@
 defmodule TestUtils do
-    def clean_string(string) do
+  def clean_string(string) do
     # Replaces non-breaking space with regular space
     # Collapses all whitespace into a single space
     # Trims leading/trailing spaces
     string
-        |> String.replace("\u00A0", " ")
-        |> String.replace(~r/\s+/, " ")
-        |> String.trim()
-    end
+    |> String.replace("\u00A0", " ")
+    |> String.replace(~r/\s+/, " ")
+    |> String.trim()
+  end
 end
