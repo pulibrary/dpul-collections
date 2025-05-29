fc068ea1cc47f66601214ccd917821680da7abb8
hackartisan <845363+hackartisan@users.noreply.github.com>
2025-05-29 10:55:47 -0400

    Lighten the colors on the home page in particular, use sage as primary
    refs #488

--- lib/dpul_collections_web/live/home_live.ex
+++ lib/dpul_collections_web/live/home_live.ex
@@ -180,7 +180,7 @@ defmodule DpulCollectionsWeb.HomeLive do
       </div>
       <.content_separator />
 
-      <div class="recent-items grid-row bg-secondary">
+      <div class="recent-items grid-row bg-background">
         <div class="content-area">
           <div class="page-t-padding" />
           <h1>{gettext("Recently Added Items")}</h1>
