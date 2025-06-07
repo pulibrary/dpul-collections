8891928b65737bea2f835bc7109314d8da0a14c9
Trey Pendragon <tpendragon@princeton.edu>
2025-06-07 14:25:49 -0700

    Add a query classifier using a tiny embedding model.
    Proof of concept for being able to go from human language query to
    search results cheaply and scalably.

--- lib/dpul_collections/application.ex
+++ lib/dpul_collections/application.ex
@@ -23,7 +23,8 @@ defmodule DpulCollections.Application do
         # {DpulCollections.Worker, arg},
         # Start to serve requests, typically the last entry
         DpulCollectionsWeb.Endpoint,
-        DpulCollections.IndexMetricsTracker
+        DpulCollections.IndexMetricsTracker,
+        DpulCollections.Classifier
       ] ++ filter_pipeline_children()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
