282373e07f94d4755ff3aa81f5cb9bf5c62bb0a8
Trey Pendragon <tpendragon@princeton.edu>
2025-06-14 20:13:32 -0400

    Format, fix parallelization problems.

--- mix.exs
+++ mix.exs
@@ -89,7 +89,7 @@ defmodule DpulCollections.MixProject do
       {:iconify_ex, "~> 0.6.1"},
       # React for Clover Viewer
       {:phoenix_live_react, "~> 0.6"},
-      {:sham, "~> 1.0", only: :test}
+      {:sham, "~> 1.0", only: :test},
       {:scholar, "~> 0.3.0"}
     ]
   end
