4e4df5645163afd0a1ab0aa6d79af08dd66491e2
Trey Pendragon <tpendragon@princeton.edu>
2025-06-23 00:39:01 -0700

    Servings based classifier.

--- config/config.exs
+++ config/config.exs
@@ -107,4 +107,4 @@ config :iconify_ex,
 import Config
 config :nx, :default_backend, EXLA.Backend
 # Client can also be set to :cuda / :rocm
-config :nx, :default_defn_options, [compiler: EXLA, client: :host]
+config :nx, :default_defn_options, compiler: EXLA, client: :host
