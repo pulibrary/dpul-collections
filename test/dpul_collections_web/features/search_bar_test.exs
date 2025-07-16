ae1c6af53fdfe9f62447c483fac34a4a520da8d0
hackartisan <845363+hackartisan@users.noreply.github.com>
2025-07-16 12:03:02 -0400

    Fix tests with search button submission

--- test/dpul_collections_web/features/search_bar_test.exs
+++ test/dpul_collections_web/features/search_bar_test.exs
@@ -1,6 +1,7 @@
 defmodule DpulCollectionsWeb.Features.SearchBarTest do
   use ExUnit.Case
   use PhoenixTest.Playwright.Case
+  alias PhoenixTest.Playwright
   import SolrTestSupport
   alias DpulCollections.Solr
 
@@ -10,21 +11,20 @@ defmodule DpulCollectionsWeb.Features.SearchBarTest do
     on_exit(fn -> Solr.delete_all(active_collection()) end)
   end
 
+  # Because the search button is only visible when the input is focused, we use
+  # type instead of fill_in
   test "submitting a search via the search bar component on the search page", %{conn: conn} do
     conn
     |> visit("/search")
-    |> fill_in("Search", with: "Document-3")
-    |> within(".search-bar", fn session ->
-      session
-      |> click_button("Search")
-    end)
+    |> Playwright.type("input#q", "Document-3")
+    |> click_button("Search")
     |> assert_has("#item-counter", text: "1 - 1 of 1")
   end
 
   test "submitting a search on the home page", %{conn: conn} do
     conn
     |> visit("/")
-    |> fill_in("Search", with: "Document-3")
+    |> Playwright.type("input#q", "Document-3")
     |> click_button("Search")
     |> assert_has("#item-counter", text: "1 - 1 of 1")
   end
