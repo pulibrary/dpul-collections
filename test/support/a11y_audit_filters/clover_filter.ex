182540d8c146d04a5a3e636924c7d10e078b5e0a
hackartisan <845363+hackartisan@users.noreply.github.com>
2025-07-16 13:03:09 -0400

    Add another clover violation to filter

--- test/support/a11y_audit_filters/clover_filter.ex
+++ test/support/a11y_audit_filters/clover_filter.ex
@@ -1,9 +1,9 @@
-defmodule RoleImgAltFilter do
+defmodule CloverFilter do
   @behaviour A11yAudit.ViolationFilter
 
   @impl A11yAudit.ViolationFilter
   def exclude_violation?(%A11yAudit.Results.Violation{} = violation) do
-    ids_to_ignore = ["role-img-alt"]
+    ids_to_ignore = ["role-img-alt", "image-redundant-alt"]
     violation.id in ids_to_ignore
   end
 end
