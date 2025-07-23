f97f814f8cef9ff84643949a48c087d31e09fbb9
hackartisan <845363+hackartisan@users.noreply.github.com>
2025-07-23 12:36:35 -0400

    Force exact angle match by making search bar a bit less tall

--- lib/dpul_collections_web/components/search_bar_component.ex
+++ lib/dpul_collections_web/components/search_bar_component.ex
@@ -6,16 +6,16 @@ defmodule DpulCollectionsWeb.SearchBarComponent do
   def render(assigns) do
     ~H"""
     <div class="search-bar">
-      <div class="search-browse-container min-h-14 flex flex-wrap bg-search">
+      <div class="search-browse-container min-h-10 flex flex-wrap bg-search">
         <div class="search-box header-x-padding grow">
           <form id="search-form" class="w-full h-full" phx-submit="search" phx-target={@myself}>
             <div class="flex items-center w-full h-full" role="search">
               <span class="flex-none">
-                <.icon name="hero-magnifying-glass" class="h-10 w-10 icon" />
+                <.icon name="hero-magnifying-glass" class="h-8 w-8 icon" />
               </span>
               <label for="q" class="sr-only">{gettext("Search")}</label>
               <input
-                class="m-2 p-1 grow h-full placeholder:text-dark-text/40 bg-transparent border-none placeholder:text-2xl text-2xl placeholder:font-bold w-full"
+                class="m-2 px-1 py-0 grow h-full placeholder:text-dark-text/40 bg-transparent border-none placeholder:text-xl text-xl placeholder:font-bold w-full"
                 type="text"
                 id="q"
                 name="q"
@@ -38,7 +38,7 @@ defmodule DpulCollectionsWeb.SearchBarComponent do
           role="navigation"
         >
           <div class="w-full text-right heading text-xl font-bold">
-            <span><.icon name="hero-square-3-stack-3d" class="p-1 h-10 w-10 icon" /></span>
+            <span><.icon name="hero-square-3-stack-3d" class="p-1 h-8 w-8 icon" /></span>
             <.link navigate={~p"/browse"} class="pl-2">
               {gettext("Explore")}
             </.link>
