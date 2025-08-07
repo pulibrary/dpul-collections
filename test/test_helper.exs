61a20275c346911c13808d17f720c6ee44558137
Trey Pendragon <tpendragon@princeton.edu>
2025-08-07 12:47:02 -0700

    Setup FiggyRepo to handle async testing.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,5 @@
 ExUnit.start(exclude: [:skip])
 Ecto.Adapters.SQL.Sandbox.mode(DpulCollections.Repo, :manual)
+Ecto.Adapters.SQL.Sandbox.mode(DpulCollections.FiggyRepo, :manual)
 
 Application.put_env(:phoenix_test, :base_url, DpulCollectionsWeb.Endpoint.url())
