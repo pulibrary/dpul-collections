361b0731cbe960b64c25791498965a03061cd3f9
Trey Pendragon <tpendragon@princeton.edu>
2025-08-15 09:50:48 -0700

    Clean up dialog interaction.

--- lib/dpul_collections_web/content_warnings.ex
+++ lib/dpul_collections_web/content_warnings.ex
@@ -46,8 +46,7 @@ defmodule DpulCollectionsWeb.ContentWarnings do
       <.link
         id={"open-show-image-banner-#{@item_id}"}
         class="flex gap-2 align-center text-rust"
-        data-dialog-id={"show-image-banner-#{@item_id}-dialog"}
-        phx-hook="OpenDialogHook"
+        phx-click={JS.exec("phx-open", to: "#show-image-banner-#{@item_id}-dialog")}
       >
         <span class="flex-none">
           <.icon name="hero-eye-slash" class="h-5 w-5 icon mb-[2px]" />
@@ -56,46 +55,55 @@ defmodule DpulCollectionsWeb.ContentWarnings do
           {gettext("Why are the images blurred?")}
         </span>
       </.link>
-      <!-- Modal content -->
-      <dialog
-        id={"show-image-banner-#{@item_id}-dialog"}
-        class="max-w-2xl backdrop:bg-black/50 open:top-[50%] open:left-[50%] open:-translate-x-50 open:-translate-y-50 fixed bg-white rounded-lg shadow-sm text-dark-text"
-        aria-labelledby={"show-image-modal-#{@item_id}-title"}
-        closedBy="any"
-      >
-        <!-- Modal header -->
-        <div class="flex items-center justify-between p-6 pb-0 rounded-t">
-          <h2 id={"show-image-modal-#{@item_id}-title"} class="text-3xl font-semibold">
-            Content Warning
-          </h2>
-          <button
-            id={"show-image-banner-close-button-#{@item_id}"}
-            type="button"
-            class="bg-transparent hover:bg-gray-200 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            data-dialog-id={"show-image-banner-#{@item_id}-dialog"}
-            phx-hook="CloseDialogHook"
-          >
-            <svg
-              class="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-              />
-            </svg>
-            <span class="sr-only">{gettext("Close modal")}</span>
-          </button>
-        </div>
-        <.content_warning_body {assigns} />
-      </dialog>
     </div>
+    <.modal {assigns} />
+    """
+  end
+
+  def modal(assigns) do
+    ~H"""
+    <!-- Modal content -->
+    <dialog
+      id={"show-image-banner-#{@item_id}-dialog"}
+      class="modal max-w-2xl backdrop:bg-black/50 open:top-[50%] open:left-[50%] open:-translate-x-50 open:-translate-y-50 fixed bg-white rounded-lg shadow-sm text-dark-text"
+      aria-labelledby={"show-image-modal-#{@item_id}-title"}
+      closedBy="any"
+      phx-open={JS.dispatch("dpulc:showDialog")}
+      phx-close={JS.dispatch("dpulc:closeDialog")}
+      phx-remove={JS.exec("phx-close")}
+      phx-hook="Dialog"
+    >
+      <!-- Modal header -->
+      <div class="flex items-center justify-between p-6 pb-0 rounded-t">
+        <h2 id={"show-image-modal-#{@item_id}-title"} class="text-3xl font-semibold">
+          Content Warning
+        </h2>
+        <button
+          id={"show-image-banner-close-button-#{@item_id}"}
+          type="button"
+          class="bg-transparent hover:bg-gray-200 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+          phx-click={JS.exec("phx-close", to: {:closest, "dialog"})}
+        >
+          <svg
+            class="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+          <span class="sr-only">{gettext("Close modal")}</span>
+        </button>
+      </div>
+      <.content_warning_body {assigns} />
+    </dialog>
     """
   end
 
@@ -140,8 +148,6 @@ defmodule DpulCollectionsWeb.ContentWarnings do
           JS.dispatch("dpulc:showImages")
           |> JS.push("show_images", value: %{id: @item_id})
         }
-        data-id={@item_id}
-        data-dialog-id={"show-image-banner-#{@item_id}-dialog"}
       >
         {gettext("View content")}
       </.primary_button>
