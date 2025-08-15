1e5e61a681551499d4ca9fd0b755f99ec0fad249
Trey Pendragon <tpendragon@princeton.edu>
2025-08-15 14:16:09 -0700

    Make this safer.

--- priv/repo/migrations/20250815174618_adjust_related_ids_index.exs
+++ priv/repo/migrations/20250815174618_adjust_related_ids_index.exs
@@ -2,16 +2,16 @@ defmodule DpulCollections.Repo.Migrations.AdjustRelatedIdsIndex do
   use Ecto.Migration
 
   def change do
-    drop index(:figgy_hydration_cache_entries, [:related_ids, :cache_version],
+    drop_if_exists index(:figgy_hydration_cache_entries, [:related_ids, :cache_version],
            name: :figgy_hydration_related_ids_cache_version_idx
          )
 
-    create index(:figgy_hydration_cache_entries, [:related_ids],
+    create_if_not_exists index(:figgy_hydration_cache_entries, [:related_ids],
              name: :figgy_hydration_related_ids_idx,
              using: "GIN"
            )
 
-    create index(:figgy_hydration_cache_entries, [:source_cache_order])
-    create index(:figgy_hydration_cache_entries, [:cache_version, :source_cache_order])
+    create_if_not_exists index(:figgy_hydration_cache_entries, [:source_cache_order])
+    create_if_not_exists index(:figgy_hydration_cache_entries, [:cache_version, :source_cache_order])
   end
 end
