c7dc51411122c0474a15c7d25f255e42461c8c9d
Trey Pendragon <tpendragon@princeton.edu>
2025-08-15 12:27:37 -0700

    Increase queue_target for test.

--- config/test.exs
+++ config/test.exs
@@ -15,7 +15,8 @@ config :dpul_collections, DpulCollections.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2,
   # in ms, 30 min, allows dbg in tests
-  ownership_timeout: 1_800_000
+  ownership_timeout: 1_800_000,
+  queue_target: 5000
 
 # Configure your other database
 config :dpul_collections, DpulCollections.FiggyRepo,
@@ -28,7 +29,8 @@ config :dpul_collections, DpulCollections.FiggyRepo,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10,
   # in ms, 30 min, allows dbg in tests
-  ownership_timeout: 1_800_000
+  ownership_timeout: 1_800_000,
+  queue_target: 5000
 
 # Playwright feature tests require the server to be running
 config :dpul_collections, DpulCollectionsWeb.Endpoint,
