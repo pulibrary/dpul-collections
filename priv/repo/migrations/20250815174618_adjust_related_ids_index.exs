8dcc06e093b8fb488169214bea2a93e852f3fa30
Trey Pendragon <tpendragon@princeton.edu>
2025-08-15 14:19:49 -0700

    Format.

--- priv/repo/migrations/20250815174618_adjust_related_ids_index.exs
+++ priv/repo/migrations/20250815174618_adjust_related_ids_index.exs
@@ -3,15 +3,19 @@ defmodule DpulCollections.Repo.Migrations.AdjustRelatedIdsIndex do
 
   def change do
     drop_if_exists index(:figgy_hydration_cache_entries, [:related_ids, :cache_version],
-           name: :figgy_hydration_related_ids_cache_version_idx
-         )
+                     name: :figgy_hydration_related_ids_cache_version_idx
+                   )
 
     create_if_not_exists index(:figgy_hydration_cache_entries, [:related_ids],
-             name: :figgy_hydration_related_ids_idx,
-             using: "GIN"
-           )
+                           name: :figgy_hydration_related_ids_idx,
+                           using: "GIN"
+                         )
 
     create_if_not_exists index(:figgy_hydration_cache_entries, [:source_cache_order])
-    create_if_not_exists index(:figgy_hydration_cache_entries, [:cache_version, :source_cache_order])
+
+    create_if_not_exists index(:figgy_hydration_cache_entries, [
+                           :cache_version,
+                           :source_cache_order
+                         ])
   end
 end
