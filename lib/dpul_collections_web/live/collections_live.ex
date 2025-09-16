ec820d58695afe4a6affe4f3ed9565097854c7f3
Trey Pendragon <tpendragon@princeton.edu>
2025-09-16 11:52:57 -0700

    Use real items for the mosaic thing, use real IDs so staging might work.

--- lib/dpul_collections_web/live/collections_live.ex
+++ lib/dpul_collections_web/live/collections_live.ex
@@ -1,4 +1,5 @@
 defmodule DpulCollectionsWeb.CollectionsLive do
+  alias DpulCollectionsWeb.BrowseItem
   use DpulCollectionsWeb, :live_view
   use Gettext, backend: DpulCollectionsWeb.Gettext
   import DpulCollectionsWeb.BrowseItem
@@ -44,7 +45,7 @@ defmodule DpulCollectionsWeb.CollectionsLive do
   defp get_recent_collection_items do
     [
       %{
-        id: "sae001",
+        id: "77ba4ea4-ce03-4a63-b0ee-a1a5b9cd746c",
         title: ["Migration from North-Eastern region to Bangalore: level and trend analysis"],
         date: "2016",
         geographic_origin: "India",
@@ -57,11 +58,11 @@ defmodule DpulCollectionsWeb.CollectionsLive do
         primary_thumbnail_width: 350,
         primary_thumbnail_height: 350,
         genre: "Reports",
-        url: "/item/sae001",
+        url: "/item/77ba4ea4-ce03-4a63-b0ee-a1a5b9cd746c",
         updated_at: "2024-01-15T10:30:00Z"
       },
       %{
-        id: "sae002",
+        id: "9bb5fedf-bdde-4207-abd3-1ade3e190d94",
         title: ["Please fasten your seat belts! We are passing through turbulent weather"],
         date: "2011",
         geographic_origin: "India",
@@ -74,11 +75,11 @@ defmodule DpulCollectionsWeb.CollectionsLive do
         primary_thumbnail_width: 350,
         primary_thumbnail_height: 350,
         genre: "Brochures",
-        url: "/item/sae002",
+        url: "/item/9bb5fedf-bdde-4207-abd3-1ade3e190d94",
         updated_at: "2024-01-12T14:22:00Z"
       },
       %{
-        id: "sae003",
+        id: "ec477e9c-eff1-4945-b438-e5e7fdcb55f9",
         title: ["Qissa soi storytelling: behrupiya storytelling tradition of Delhi"],
         date: "2018",
         geographic_origin: "India",
@@ -91,24 +92,23 @@ defmodule DpulCollectionsWeb.CollectionsLive do
         primary_thumbnail_width: 350,
         primary_thumbnail_height: 350,
         genre: "Flyers",
-        url: "/item/sae003",
+        url: "/item/ec477e9c-eff1-4945-b438-e5e7fdcb55f9",
         updated_at: "2024-01-18T09:15:00Z"
       },
       %{
-        id: "sae004",
-        title: ["Pokhara Darpan: Annual Bulletin, Issue 17, 2068 Bhadra, September 2011"],
-        date: "2011",
-        geographic_origin: "Nepal",
-        file_count: 67,
+        id: "6fb2af84-6a4f-401d-8c7e-5787633876f5",
+        title: ["تغیر پذیر حالت میں تفقه کے تقاضے اور هم"],
+        geographic_origin: "India",
+        file_count: 22,
         primary_thumbnail_service_url:
-          "https://iiif-cloud.princeton.edu/iiif/2/38%2F4e%2Fec%2F384eec1c6fcc4229be4acb2169b5ba29%2Fintermediate_file",
+          "https://iiif-cloud.princeton.edu/iiif/2/e1%2F39%2Fe6%2Fe139e69b8421409392c35fdf936d8895%2Fintermediate_file",
         image_service_urls: [
-          "https://iiif-cloud.princeton.edu/iiif/2/38%2F4e%2Fec%2F384eec1c6fcc4229be4acb2169b5ba29%2Fintermediate_file"
+          "https://iiif-cloud.princeton.edu/iiif/2/e1%2F39%2Fe6%2Fe139e69b8421409392c35fdf936d8895%2Fintermediate_file"
         ],
         primary_thumbnail_width: 350,
         primary_thumbnail_height: 350,
-        genre: "Serials",
-        url: "/item/sae004",
+        genre: "Booklets",
+        url: "/item/6fb2af84-6a4f-401d-8c7e-5787633876f5",
         updated_at: "2024-01-10T16:45:00Z"
       }
     ]
@@ -116,45 +116,7 @@ defmodule DpulCollectionsWeb.CollectionsLive do
   end
 
   defp get_featured_items do
-    # Get a selection of visually interesting items for the hero section
-    [
-      %{
-        id: "sae001",
-        image_url:
-          "https://iiif-cloud.princeton.edu/iiif/2/a2%2F30%2F20%2Fa23020f89dd645f1803be45dc9ff0d17%2Fintermediate_file/full/!350,350/0/default.jpg",
-        title: "Migration from North-Eastern region to Bangalore: level and trend analysis"
-      },
-      %{
-        id: "sae002",
-        image_url:
-          "https://iiif-cloud.princeton.edu/iiif/2/7e%2F67%2F0e%2F7e670ec857c94ca5a6b2c4e195daaa9d%2Fintermediate_file/full/!350,350/0/default.jpg",
-        title: "Please fasten your seat belts! We are passing through turbulent weather"
-      },
-      %{
-        id: "sae003",
-        image_url:
-          "https://iiif-cloud.princeton.edu/iiif/2/6f%2F85%2Fde%2F6f85deaa645d480d8564916ac887be9a%2Fintermediate_file/full/!350,350/0/default.jpg",
-        title: "Qissa soi storytelling: behrupiya storytelling tradition of Delhi"
-      },
-      %{
-        id: "sae004",
-        image_url:
-          "https://iiif-cloud.princeton.edu/iiif/2/38%2F4e%2Fec%2F384eec1c6fcc4229be4acb2169b5ba29%2Fintermediate_file/full/!350,350/0/default.jpg",
-        title: "Pokhara Darpan: Annual Bulletin, Issue 17, 2068 Bhadra, September 2011"
-      },
-      %{
-        id: "sae005",
-        image_url:
-          "https://iiif-cloud.princeton.edu/iiif/2/a2%2F30%2F20%2Fa23020f89dd645f1803be45dc9ff0d17%2Fintermediate_file/full/!350,350/0/default.jpg",
-        title: "Migration from North-Eastern region to Bangalore: level and trend analysis"
-      },
-      %{
-        id: "sae006",
-        image_url:
-          "https://iiif-cloud.princeton.edu/iiif/2/38%2F4e%2Fec%2F384eec1c6fcc4229be4acb2169b5ba29%2Fintermediate_file/full/!350,350/0/default.jpg",
-        title: "Pokhara Darpan: Annual Bulletin, Issue 17, 2068 Bhadra, September 2011"
-      }
-    ]
+    get_recent_collection_items()
   end
 
   defp get_categories do
@@ -408,12 +370,13 @@ defmodule DpulCollectionsWeb.CollectionsLive do
                         2 -> "translate-y-6"
                       end
                     ]}>
-                      <.link navigate={"/item/#{item.id}"}>
-                        <img
-                          src={item.image_url}
-                          alt={item.title}
-                          class="w-full h-40 object-cover object-top rounded-t"
-                        />
+                      <.link navigate={item.url}>
+                        <div class="w-full h-40 object-cover object-top rounded-t overflow-hidden">
+                          <BrowseItem.thumb
+                            item={item}
+                            thumb={BrowseItem.thumbnail_service_url(item)}
+                          />
+                        </div>
                         <div class="p-2 bg-background">
                           <p class="text-xs font-medium truncate">{item.title}</p>
                         </div>
