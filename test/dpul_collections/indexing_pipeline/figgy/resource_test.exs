6561709ae00aed792d50c3d326a463a0ac9589b3
Trey Pendragon <tpendragon@princeton.edu>
2025-09-17 13:32:25 -0700

    Fetch categories into hydration data.

--- test/dpul_collections/indexing_pipeline/figgy/resource_test.exs
+++ test/dpul_collections/indexing_pipeline/figgy/resource_test.exs
@@ -15,4 +15,15 @@ defmodule DpulCollections.IndexingPipeline.Figgy.ResourceTest do
       assert doc[:genre_txtm] == ["Ephemera"]
     end
   end
+
+  describe ".to_combined()" do
+    test "it grabs vocabularies/categories into related_data" do
+      folder = IndexingPipeline.get_figgy_resource!("26713a31-d615-49fd-adfc-93770b4f66b3")
+
+      combined_resource = folder |> Figgy.Resource.to_combined()
+
+      refute combined_resource.related_data["resources"]["277cdbea-c0a8-4b7f-8bf6-de5ac07f95c3"] ==
+               nil
+    end
+  end
 end
