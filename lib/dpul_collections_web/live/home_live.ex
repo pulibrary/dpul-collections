0d24c58038a7954459284f75b6aeb19677e3d478
Trey Pendragon <tpendragon@princeton.edu>
2025-10-01 14:47:08 -0700

    Shot at a new home page callout.
    I had to get this out of my head.

--- lib/dpul_collections_web/live/home_live.ex
+++ lib/dpul_collections_web/live/home_live.ex
@@ -30,28 +30,24 @@ defmodule DpulCollectionsWeb.HomeLive do
     <Layouts.app flash={@flash} content_class={}>
       <div class="grid grid-flow-row auto-rows-max">
         <div class="explore-header grid-row bg-background relative">
-          <div class="drop-shadow-[1px_1px_3rem_rgba(0,0,0,1)] bg-primary absolute max-h-[600px] sm:min-w-[350px] w-full lg:max-w-1/2 2xl:max-w-1/3 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 p-4">
-            <div class="corner-cut content-area text-center h-full w-full flex flex-col justify-evenly bg-background p-8">
-              <div class="page-y-padding text-2xl flex-grow">
-                {gettext(
-                  "Discover %{photographs}, %{posters}, %{pamphlets}, and more to inspire your research",
-                  photographs:
-                    callout_link(%{
-                      url: ~p"/search?#{%{filter: %{genre: "photographs"}}}",
-                      label: gettext("photographs")
-                    }),
-                  posters:
-                    callout_link(%{
-                      url: ~p"/search?#{%{filter: %{genre: "posters"}}}",
-                      label: gettext("posters")
-                    }),
-                  pamphlets:
-                    callout_link(%{
-                      url: ~p"/search?#{%{filter: %{genre: "pamphlets"}}}",
-                      label: gettext("pamphlets")
-                    })
-                )
-                |> Phoenix.HTML.raw()}
+          <div class="drop-shadow-[1px_1px_3rem_rgba(0,0,0,1)] bg-primary absolute max-h-[600px] sm:min-w-[350px] w-full lg:max-w-2/3 2xl:max-w-1/3 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 p-4">
+            <div class="corner-cut content-area text-center h-full w-full flex flex-col justify-evenly items-center bg-background p-8 gap-4">
+              <h1 class="text-4xl">Welcome to the Curious and Passionate</h1>
+              <div class="flex flex-wrap gap-4">
+                <div class="flex items-center text-dark-text gap-2">
+                  <div class="bg-light-accent rounded-full px-3 py-1">
+                    38,504 Items
+                  </div>
+                  <div class="bg-light-accent rounded-full px-3 py-1">
+                    86 Languages
+                  </div>
+                  <div class="bg-light-accent rounded-full px-3 py-1">
+                    274 Locations
+                  </div>
+                </div>
+              </div>
+              <div class="text-2xl flex-grow">
+                Browse six continents and six millenia of free publically available books, manuscripts, objects and art organized into collections for you to use, download, share, and enjoy.
               </div>
               <div class="content-area bg-primary text-light-text px-0 text-xl">
                 <.primary_button href={~p"/browse"}>
