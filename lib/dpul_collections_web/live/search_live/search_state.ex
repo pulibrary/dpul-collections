8b1304a6a1a713d751aea6a1a3fae385dea280eb
Trey Pendragon <tpendragon@princeton.edu>
2025-10-09 14:00:00 -0700

    Fix removing facets.

--- lib/dpul_collections_web/live/search_live/search_state.ex
+++ lib/dpul_collections_web/live/search_live/search_state.ex
@@ -13,6 +13,29 @@ defmodule DpulCollectionsWeb.SearchLive.SearchState do
     }
   end
 
+  def remove_filter_value(search_state = %{filter: filters}, filter, value)
+      when is_map_key(filters, filter) do
+    cond do
+      # There's a list, make sure the given value is gone and return the state
+      # without it.
+      is_list(filters[filter]) ->
+        search_state
+        |> put_in([:filter, filter], List.delete(filters[filter], value))
+
+      true ->
+        search_state
+    end
+  end
+
+  def remove_filter_value(search_state, _, _) do
+    search_state
+  end
+
+  def set_filter(search_state, filter, value) do
+    search_state
+    |> put_in([:filter, filter], value)
+  end
+
   defp valid_sort_by(%{"sort_by" => sort_by})
        when sort_by in @sort_by_keys do
     String.to_existing_atom(sort_by)
