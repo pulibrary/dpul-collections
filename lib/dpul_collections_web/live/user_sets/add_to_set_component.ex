8d2678fef278244f772029698895bbc696677bff
Trey Pendragon <tpendragon@princeton.edu>
2025-10-17 11:05:04 -0700

    Try making things easier to read.

--- lib/dpul_collections_web/live/user_sets/add_to_set_component.ex
+++ lib/dpul_collections_web/live/user_sets/add_to_set_component.ex
@@ -5,19 +5,31 @@ defmodule DpulCollectionsWeb.UserSets.AddToSetComponent do
   use DpulCollectionsWeb, :live_component
   use Gettext, backend: DpulCollectionsWeb.Gettext
 
+  # Called by live_component when this spins up - use it to pre-populate
+  # starting state via #display_list_sets().
   def update(assigns, socket) do
     {
       :ok,
       socket
       |> assign(assigns)
-      |> reset()
+      |> display_list_sets()
     }
   end
 
   attr :item_id, :string, default: nil
   attr :current_scope, :any
-  attr :mode, :string, default: "append"
 
+  attr :mode, :atom,
+    doc: "a signifier for which part of the add to set modal to show",
+    values: [:list_sets, :new_set],
+    default: :list_sets
+
+  @doc """
+  Displays a modal to allow adding a given Item ID to a user's Set.
+  There are two modes:
+    - list_sets (Lists all a user's sets, allows a user to click any to add that item or click 'Create New Set')
+    - new_set (Create a new set and add the item to it)
+  """
   def render(assigns) do
     ~H"""
     <div id={@id}>
@@ -27,45 +39,34 @@ defmodule DpulCollectionsWeb.UserSets.AddToSetComponent do
         label="Save to Set"
       >
         <div id="add-set-modal-content" class="min-w-[400px] mt-4 w-full flex">
-          <.append :if={@mode == "append"} {assigns} />
-          <.new_set_form :if={@mode == "new_set"} {assigns} />
+          <.list_sets :if={@mode == :list_sets} {assigns} />
+          <.new_set :if={@mode == :new_set} {assigns} />
         </div>
       </.modal>
     </div>
     """
   end
 
-  def new_set_form(assigns) do
-    ~H"""
-    <.form
-      for={@set_form}
-      class="flex flex-col gap-2 w-full"
-      phx-submit="save_new_set"
-      phx-target={@myself}
-    >
-      <.input type="text" required={true} label="Set name" field={@set_form[:title]} />
-      <.input type="textarea" label="Set description" field={@set_form[:description]} />
-      <.inputs_for :let={si_form} field={@set_form[:set_items]}>
-        <input type="hidden" name={si_form[:solr_id].name} value={si_form[:solr_id].value} />
-      </.inputs_for>
-      <div class="flex w-full">
-        <.secondary_button phx-click="reset" phx-target={@myself}>
-          Cancel
-        </.secondary_button>
-        <div class="grow"></div>
-        <.primary_button>
-          Create Set
-        </.primary_button>
-      </div>
-    </.form>
-    """
+  @doc """
+  Resets the modal back to list_sets view.
+  """
+  def display_list_sets(socket) do
+    socket
+    |> assign(:mode, :list_sets)
+    |> assign(
+      :sets,
+      UserSets.list_user_sets_for_addition(socket.assigns.current_scope, socket.assigns[:item_id])
+    )
   end
 
-  def append(assigns) do
+  @doc """
+  List a user's sets and allow them to append the item to one - or click to create a new one.
+  """
+  def list_sets(assigns) do
     ~H"""
     <div class="w-full flex flex-col gap-2">
       <.primary_button
-        phx-click="create_set"
+        phx-click="display_new_set_form"
         phx-target={@myself}
         class="w-full text-left"
       >
@@ -78,7 +79,7 @@ defmodule DpulCollectionsWeb.UserSets.AddToSetComponent do
           <.secondary_button
             class="group-[.has-item]:bg-loud-button group-[.has-item]:text-light-text group-[.has-item]:hover:text-dark-text w-full flex text-left items-left justify-left"
             phx-target={@myself}
-            phx-click="append_item"
+            phx-click="create_set_item"
             phx-value-set-id={set.id}
           >
             <span class="grow">
@@ -92,8 +93,36 @@ defmodule DpulCollectionsWeb.UserSets.AddToSetComponent do
     """
   end
 
+  @doc """
+  """
+  def new_set(assigns) do
+    ~H"""
+    <.form
+      for={@set_form}
+      class="flex flex-col gap-2 w-full"
+      phx-submit="create_set"
+      phx-target={@myself}
+    >
+      <.input type="text" required={true} label="Set name" field={@set_form[:title]} />
+      <.input type="textarea" label="Set description" field={@set_form[:description]} />
+      <.inputs_for :let={si_form} field={@set_form[:set_items]}>
+        <input type="hidden" name={si_form[:solr_id].name} value={si_form[:solr_id].value} />
+      </.inputs_for>
+      <div class="flex w-full">
+        <.secondary_button phx-click="display_list_sets" phx-target={@myself}>
+          Cancel
+        </.secondary_button>
+        <div class="grow"></div>
+        <.primary_button>
+          Create Set
+        </.primary_button>
+      </div>
+    </.form>
+    """
+  end
+
   def handle_event(
-        "append_item",
+        "create_set_item",
         %{"set-id" => set_id},
         socket = %{assigns: %{current_scope: current_scope, item_id: solr_id}}
       ) do
@@ -109,11 +138,11 @@ defmodule DpulCollectionsWeb.UserSets.AddToSetComponent do
         {:ok, _} = UserSets.delete_set_item(set_item)
     end
 
-    {:noreply, socket |> reset()}
+    {:noreply, socket |> display_list_sets()}
   end
 
   def handle_event(
-        "save_new_set",
+        "create_set",
         %{"set" => params},
         socket = %{assigns: %{current_scope: current_scope}}
       ) do
@@ -121,7 +150,7 @@ defmodule DpulCollectionsWeb.UserSets.AddToSetComponent do
       {:ok, _set} ->
         {:noreply,
          socket
-         |> reset()}
+         |> display_list_sets()}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, new_set_form: to_form(changeset))}
@@ -129,7 +158,7 @@ defmodule DpulCollectionsWeb.UserSets.AddToSetComponent do
   end
 
   def handle_event(
-        "create_set",
+        "display_new_set_form",
         _params,
         socket = %{assigns: %{current_scope: current_scope, item_id: item_id}}
       ) do
@@ -145,7 +174,7 @@ defmodule DpulCollectionsWeb.UserSets.AddToSetComponent do
           })
         )
       )
-      |> assign(:mode, "new_set")
+      |> assign(:mode, :new_set)
     }
   end
 
@@ -153,30 +182,26 @@ defmodule DpulCollectionsWeb.UserSets.AddToSetComponent do
     {:noreply,
      socket
      |> assign(:item_id, item_id)
-     |> reset()
+     |> display_list_sets()
      # Notes to self:
      # We can get rid of a bunch of this Modal stuff with
      # JS.ignore_attribute on phx-mounted
      |> push_event("dcjs-open", %{id: "add-set-modal"})}
   end
 
-  def handle_event("reset", _, socket) do
+  def handle_event("display_list_sets", _, socket) do
     {:noreply,
      socket
-     |> reset()}
+     |> display_list_sets()}
   end
 
-  def reset(socket) do
-    socket
-    |> assign(:mode, "append")
-    |> assign(
-      :sets,
-      UserSets.list_user_sets_for_addition(socket.assigns.current_scope, socket.assigns[:item_id])
-    )
-  end
+  attr :item_id, :string,
+    doc: "Item ID to be added to a user set if this button is clicked. Should be a Solr ID.",
+    required: true
 
-  attr :item_id, :string, required: true
-
+  @doc """
+  Function component that triggers the add to set modal for a given Item ID when it's clicked.
+  """
   def add_button(assigns) do
     ~H"""
     <.card_button
