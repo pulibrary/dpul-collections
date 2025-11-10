63f3207ded8720655d8127084df3cc1d6d404b78
Oh, Hey! <shaun@sdellis.com>
2025-11-10 16:33:16 -0500

    additional changes to hero layout

--- lib/dpul_collections_web/live/collections_live.ex
+++ lib/dpul_collections_web/live/collections_live.ex
@@ -71,41 +71,43 @@ defmodule DpulCollectionsWeb.CollectionsLive do
       <div class="home-content-area grid grid-flow-row auto-rows-max">
         <!-- Hero Section -->
         <div class="relative overflow-hidden">
-          <div class="home-content-area left-[25%] absolute z-10">
+          <div class="home-content-area left-[10%] absolute z-10">
             <img src="/images/triangle-mosaic.png" alt='' class="mx-auto w-xl" />
           </div>
           <div class="home-content-area space-y-2 relative z-30">
             <p class="text-accent font-semibold text-xl uppercase tracking-wide">
               Digital Collection
             </p>
-            <h1 class="flex-grow-1 text-4xl lg:text-4xl font-bold">
+            <h1 class="flex-grow-1 text-4xl lg:text-6xl font-[1000]">
               {@collection.title |> hd}
             </h1>
           </div>
           <div class='home-content-area page-y-padding relative z-30'>
-            <div class="grid lg:grid-cols-2 gap-0">
+            <div class="grid grid-cols-2 gap-0 grid-cols-[auto_60%]">
               <!-- Left Column: Content -->
               <div>
-                <div class="bg-white opacity-50 w-full h-50 relative z-30">
-                  <div class="flex flex-wrap gap-4 p-5">
-                    <p>Foobar Foobar Foobar!</p>
+                <div class="w-full relative z-30">
+                  <div class="w-full h-full bg-white opacity-75 absolute z-40">
+                    
+                  </div>
+                  <div class="flex flex-wrap gap-4 p-5 relative z-50">
+                    <p class="text-lg font-semibold text-dark-text italic font-serif">
+                      The South Asian Ephemera Collection is an openly accessible repository of items that spans a variety of subjects and languages and supports research, teaching, and private study. Newly acquired materials are digitized and added on an ongoing basis.  
+                      {@collection.tagline}
+                    </p>
                     <div class="flex items-center text-dark-text gap-2">
-                      <div class="bg-light-accent rounded-full px-3 py-1">
+                      <div class="text-sm bg-cloud rounded-full px-3 py-1">
                         {@collection.item_count} Items
                       </div>
-                      <div class="bg-light-accent rounded-full px-3 py-1">
+                      <div class="text-sm bg-cloud rounded-full px-3 py-1">
                         {length(@collection.languages)} Languages
                       </div>
-                      <div class="bg-light-accent rounded-full px-3 py-1">
+                      <div class="text-sm bg-cloud rounded-full px-3 py-1">
                         {length(@collection.geographic_origins)} Locations
                       </div>
                     </div>
                   </div>
                 </div>
-                <p class="text-xl leading-relaxed">
-                  {@collection.tagline}
-                </p>
-
                 <div class="flex flex-wrap gap-4 pt-4">
                   <button
                     phx-click={
@@ -114,7 +116,7 @@ defmodule DpulCollectionsWeb.CollectionsLive do
                         to: "#collection-description"
                       )
                     }
-                    class="btn-secondary"
+                    class="btn-secondary bg-dark-gray text-light-text"
                   >
                     {gettext("Learn More")}
                   </button>
@@ -125,17 +127,17 @@ defmodule DpulCollectionsWeb.CollectionsLive do
                 <div class="w-full h-full relative">
                   <.primary_button
                     href={~p"/search?#{%{filter: %{project: [@collection.title |> hd]}}}"}
-                    class="btn-primary absolute bottom-[15%] right-0"
+                    class="btn-primary absolute bottom-[5%] right-0"
                   >
                     {gettext("Browse Collection")}
                   </.primary_button>
                   <%= for {item, index} <-  Enum.with_index(@collection.featured_items) do %>
                     <.link 
                       href={item.url}
-                      class={"card w-[100%] p-2 bg-background min-h-0 min-w-0 absolute z-[#{index}]"}
+                      class={"card w-[100%] p-2 bg-white min-h-0 min-w-0 absolute z-[#{index}]"}
                       aria-label={"View #{item.title |> hd}"}             
                     >
-                      <div class="max-h-80 h-full w-full overflow-hidden">
+                      <div class="max-h-90 h-full w-full overflow-hidden">
                         <img
                           src={"#{item.primary_thumbnail_service_url}/full/!#{item.primary_thumbnail_width},#{item.primary_thumbnail_height}/0/default.jpg"}
                           width={item.primary_thumbnail_width}
