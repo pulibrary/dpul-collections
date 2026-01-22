0e40f33da9fd68de25e51e4099f8d4f92960f99f
Trey Pendragon <tpendragon@princeton.edu>
2026-01-22 13:49:08 -0800

    Start of Solr 9

--- bin/setup_solr.sh
+++ bin/setup_solr.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-cd /opt/solr
-server/scripts/cloud-scripts/zkcli.sh -zkhost localhost:9983 -cmd putfile /security.json /opt/solr/security.json
+SOLR_HOST="localhost:8983"
+ZK_HOST="localhost:9983"
 
-cd /app/solr/conf
-zip -1 -r solr_config.zip ./*
+# zkcli.sh is deprecated, use zk putfile now.
+solr zk cp file:/opt/solr/security.json zk:/security.json -z $ZK_HOST
 
 # since we have to log in to the UI, add a user that's simpler to type
-curl --user solr:SolrRocks http://localhost:8983/solr/admin/authentication -H 'Content-type:application/json' -d '{"set-user": {"user":"pass"}}'
+curl --user solr:SolrRocks http://$SOLR_HOST/solr/admin/authentication \
+     -H 'Content-type:application/json' \
+     -d '{"set-user": {"user":"pass"}}'
 
-# these use solr api v2 which is not stable and changes between solr8 and solr 9
-curl -X POST "http://solr:SolrRocks@localhost:8983/solr/admin/configs?action=UPLOAD&name=dpul-collections" -H "Content-type:application/octet-stream" --data-binary @solr_config.zip
-curl -X POST http://solr:SolrRocks@localhost:8983/api/collections/ -H 'Content-type: application/json' -d '{create: {name: dpulc1, config: dpul-collections, numShards: 1}}'
-curl -X POST http://solr:SolrRocks@localhost:8983/api/c -H 'Content-Type: application/json' -d '{create-alias: {name: dpulc, collections:[dpulc1]}}'
+# Upload config with solr binary.
+/opt/solr/bin/solr zk upconfig -n dpul-collections -d /app/solr/conf
+
+# Solr 9 API
+curl -X POST "http://solr:SolrRocks@$SOLR_HOST/api/collections" \
+     -H 'Content-Type: application/json' \
+     -d '{
+       "name": "dpulc1",
+       "config": "dpul-collections",
+       "numShards": 1
+     }'
+
+curl -X POST "http://solr:SolrRocks@$SOLR_HOST/api/aliases" \
+     -H 'Content-Type: application/json' \
+     -d '{
+       "name": "dpulc",
+       "collections": ["dpulc1"]
+     }'
