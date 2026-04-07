fd0e164f5eba15a2fb9f9c758c09539d2c4cc9bb
Trey Pendragon <tpendragon@princeton.edu>
2026-04-07 11:54:12 -0700

    Remove bad comment.

--- lib/dpul_collections_web/cldr.ex
+++ lib/dpul_collections_web/cldr.ex
@@ -2,7 +2,6 @@ defmodule DpulCollectionsWeb.Cldr do
   @moduledoc """
   """
   use Cldr,
-    # according to https://github.com/elixir-cldr/cldr/issues/42
     locales: ["en", "es", "pt"],
     gettext: MyPhoenixApp.Gettext,
     providers: [Cldr.Number, Cldr.Calendar, Cldr.DateTime, Cldr.LocaleDisplay]
