31572d29c3d2dcd1c466160df9999f6ef30655cd
hackartisan <845363+hackartisan@users.noreply.github.com>
2026-04-08 10:53:48 -0400

    Search within mobile proposal

--- lib/dpul_collections_web/components/search_bar_component.ex
+++ lib/dpul_collections_web/components/search_bar_component.ex
@@ -7,28 +7,33 @@ defmodule DpulCollectionsWeb.SearchBarComponent do
     ~H"""
     <div class="search-bar cover-with-pane">
       <div class="search-browse-container min-h-10 flex flex-wrap bg-search">
-        <div class="search-box header-x-padding grow">
+        <div class="search-box header-x-padding grow w-full">
           <form id="search-form" class="group w-full h-full" phx-submit="search" phx-target={@myself}>
-            <div class="flex items-center w-full h-full space-x-2" role="search">
+            <div class="flex flex-wrap items-center w-full h-full" role="search">
+              <div class="flex items-center w-full">
               <span class="flex-none">
                 <.icon name="hero-magnifying-glass" class="h-8 w-8 icon" />
               </span>
               <label for="q" class="sr-only">{gettext("Search")}</label>
               <input
-                class="m-2 px-1 py-0 grow h-full placeholder:text-dark-text/40 bg-transparent border-none placeholder:text-xl text-xl placeholder:font-bold w-full"
+                class="m-2 px-1 py-0 grow w-full h-full placeholder:text-dark-text/40 bg-transparent border-none placeholder:text-xl text-xl placeholder:font-bold"
                 type="text"
                 id="q"
                 name="q"
                 placeholder={gettext("Search")}
                 dir="auto"
               />
+              </div>
+
+              <div class="flex items-stretch space-x-2 my-2 text-sm md:text-md w-full">
               <.primary_button
                 id="collection-search-button"
                 type="submit"
                 class="btn-primary px-4 h-8 flex-none"
               >
-                {gettext("Search in this Collection")}<.icon name="hero-arrow-turn-down-left" class="h-6/10 ml-1"/>
+                {gettext("Search in this Collection")}<span class="hidden md:inline-flex"><.icon name="hero-arrow-turn-down-left" class="invisible md:visible h-6/10 ml-1"/></span>
               </.primary_button>
+
               <.primary_button
                 id="search-button"
                 type="submit"
@@ -36,6 +41,7 @@ defmodule DpulCollectionsWeb.SearchBarComponent do
               >
                 {gettext("Search all")}
               </.primary_button>
+              </div>
             </div>
           </form>
         </div>
