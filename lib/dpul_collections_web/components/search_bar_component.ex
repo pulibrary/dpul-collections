5af2cb3f0e88ab7fa204707871ceb7b7649d7f9f
hackartisan <845363+hackartisan@users.noreply.github.com>
2026-04-09 11:23:00 -0400

    Add responsiveness between full width and mobile layouts

--- lib/dpul_collections_web/components/search_bar_component.ex
+++ lib/dpul_collections_web/components/search_bar_component.ex
@@ -10,34 +10,34 @@ defmodule DpulCollectionsWeb.SearchBarComponent do
         <div class="search-box header-x-padding grow w-full">
           <form id="search-form" class="group w-full h-full" phx-submit="search" phx-target={@myself}>
             <div class="flex flex-wrap items-center w-full h-full" role="search">
-              <div class="flex items-center w-full">
-              <span class="flex-none">
-                <.icon name="hero-magnifying-glass" class="h-8 w-8 icon" />
-              </span>
-              <label for="q" class="sr-only">{gettext("Search")}</label>
-              <input
-                class="m-2 px-1 py-0 grow w-full h-full placeholder:text-dark-text/40 bg-transparent border-none placeholder:text-xl text-xl placeholder:font-bold"
-                type="text"
-                id="q"
-                name="q"
-                placeholder={gettext("Search")}
-                dir="auto"
-              />
+              <div class="flex items-center grow">
+                <span class="flex-none">
+                  <.icon name="hero-magnifying-glass" class="h-8 w-8 icon" />
+                </span>
+                <label for="q" class="sr-only">{gettext("Search")}</label>
+                <input
+                  class="m-2 px-1 py-0 grow w-full h-full placeholder:text-dark-text/40 bg-transparent border-none placeholder:text-xl text-xl placeholder:font-bold"
+                  type="text"
+                  id="q"
+                  name="q"
+                  placeholder={gettext("Search")}
+                  dir="auto"
+                />
               </div>
 
-              <div class="flex items-stretch space-x-2 my-2 text-sm md:text-md w-full">
+              <div class="flex items-stretch space-x-2 my-2 text-sm md:text-md w-full md:w-auto">
               <.primary_button
                 id="collection-search-button"
                 type="submit"
-                class="btn-primary px-4 h-8 flex-none"
+                class="btn-primary px-4 h-8 grow md:flex-none"
               >
-                {gettext("Search in this Collection")}<span class="hidden md:inline-flex"><.icon name="hero-arrow-turn-down-left" class="invisible md:visible h-6/10 ml-1"/></span>
+                {gettext("Search in this Collection")} <.icon name="hero-arrow-turn-down-left" class="h-4/7 ml-1 hidden md:inline"/>
               </.primary_button>
 
               <.primary_button
                 id="search-button"
                 type="submit"
-                class="btn-primary px-4 h-8 flex-none mr-2px"
+                class="btn-primary px-4 h-8 grow md:flex-none mr-2px"
               >
                 {gettext("Search all")}
               </.primary_button>
