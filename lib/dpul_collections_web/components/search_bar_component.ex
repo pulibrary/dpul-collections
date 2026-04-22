f9a44e0b4bfa2d7ac1a0dd02bad0e3e03c15839c
hackartisan <845363+hackartisan@users.noreply.github.com>
2026-04-22 10:34:04 -0400

    Reduce nesting by removing one div

--- lib/dpul_collections_web/components/search_bar_component.ex
+++ lib/dpul_collections_web/components/search_bar_component.ex
@@ -8,67 +8,70 @@ defmodule DpulCollectionsWeb.SearchBarComponent do
 
   def render(assigns) do
     ~H"""
-    <div class="search-bar cover-with-pane">
-      <div class="search-browse-container min-h-10 flex flex-wrap bg-search">
-        <div class="search-box header-x-padding grow w-full">
-          <form id="search-form" class="group w-full h-full" phx-submit="search" phx-target={@myself}>
-            <.input
-              :if={Enum.any?(@search_state)}
-              type="hidden"
-              name="search_state"
-              value={JSON.encode!(@search_state)}
-            />
-            <div
-              class={[
-                "flex items-center w-full h-full",
-                String.length(@collection_title) > 0 && "flex-wrap"
-              ]}
-              role="search"
-            >
-              <div class="flex items-center grow">
-                <span class="flex-none">
-                  <.icon name="hero-magnifying-glass" class="h-8 w-8 icon" />
-                </span>
-                <label for="q" class="sr-only">{gettext("Search")}</label>
-                <input
-                  class="m-2 px-1 py-0 grow w-full h-full placeholder:text-dark-text/40 bg-transparent border-none placeholder:text-xl text-xl placeholder:font-bold"
-                  type="text"
-                  id="q"
-                  name="q"
-                  placeholder={gettext("Search")}
-                  dir="auto"
-                />
-              </div>
-
-              <div class={[
-                "flex items-stretch space-x-2 my-2 text-sm md:text-md md:w-auto",
-                String.length(@collection_title) > 0 && "w-full"
-              ]}>
-                <.primary_button
-                  :if={String.length(@collection_title) > 0}
-                  id="collection-search-button"
-                  type="submit"
-                  name="search"
-                  value={@collection_title}
-                  class="btn-primary px-4 h-8 grow"
-                >
-                  {gettext("Search in this Collection")}
-                  <.icon name="hero-arrow-turn-down-left" class="h-4/7 ml-1 hidden md:inline" />
-                </.primary_button>
-
-                <.primary_button
-                  id="search-button"
-                  type="submit"
-                  name="search"
-                  value="all"
-                  class="btn-primary px-4 h-8 mr-2px grow"
-                >
-                  {search_label(@collection_title)}
-                </.primary_button>
-              </div>
+    <div class="subnav-bar cover-with-pane">
+      <div class="search-container min-h-10 flex flex-wrap bg-search">
+        <form
+          id="search-form"
+          class="header-x-padding grow group w-full h-full"
+          phx-submit="search"
+          phx-target={@myself}
+        >
+          <.input
+            :if={Enum.any?(@search_state)}
+            type="hidden"
+            name="search_state"
+            value={JSON.encode!(@search_state)}
+          />
+          <div
+            class={[
+              "flex items-center w-full h-full",
+              String.length(@collection_title) > 0 && "flex-wrap"
+            ]}
+            role="search"
+          >
+            <div class="flex items-center grow">
+              <span class="flex-none">
+                <.icon name="hero-magnifying-glass" class="h-8 w-8 icon" />
+              </span>
+              <label for="q" class="sr-only">{gettext("Search")}</label>
+              <input
+                class="m-2 px-1 py-0 grow w-full h-full placeholder:text-dark-text/40 bg-transparent border-none placeholder:text-xl text-xl placeholder:font-bold"
+                type="text"
+                id="q"
+                name="q"
+                placeholder={gettext("Search")}
+                dir="auto"
+              />
             </div>
-          </form>
-        </div>
+
+            <div class={[
+              "flex items-stretch space-x-2 my-2 text-sm md:text-md md:w-auto",
+              String.length(@collection_title) > 0 && "w-full"
+            ]}>
+              <.primary_button
+                :if={String.length(@collection_title) > 0}
+                id="collection-search-button"
+                type="submit"
+                name="search"
+                value={@collection_title}
+                class="btn-primary px-4 h-8 grow"
+              >
+                {gettext("Search in this Collection")}
+                <.icon name="hero-arrow-turn-down-left" class="h-4/7 ml-1 hidden md:inline" />
+              </.primary_button>
+
+              <.primary_button
+                id="search-button"
+                type="submit"
+                name="search"
+                value="all"
+                class="btn-primary px-4 h-8 mr-2px grow"
+              >
+                {search_label(@collection_title)}
+              </.primary_button>
+            </div>
+          </div>
+        </form>
       </div>
       <.content_separator />
     </div>
