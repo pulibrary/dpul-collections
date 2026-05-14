8f26f00d111d341712d073528d5ad9afbec4e7f1
Trey Pendragon <tpendragon@princeton.edu>
2026-05-14 15:27:32 -0700

    Don't worry about the accuracy.

--- lib/dpul_collections/solr.ex
+++ lib/dpul_collections/solr.ex
@@ -75,6 +75,7 @@ defmodule DpulCollections.Solr do
         uf: "* _query_",
         spellcheck: true,
         "spellcheck.collate": true,
+        "spellcheck.accuracy": 0,
         "spellcheck.dictionary": "wordbreak",
         "spellcheck.dictionary": "default",
         "spellcheck.maxCollationTries": 100
