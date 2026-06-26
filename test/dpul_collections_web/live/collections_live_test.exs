4dcb4ac1a4951054dbda913d9aac1786badb58f7
Oh, Hey! <shaun@sdellis.com>
2026-06-26 10:37:02 -0400

    clean up debug code

--- test/dpul_collections_web/live/collections_live_test.exs
+++ test/dpul_collections_web/live/collections_live_test.exs
@@ -39,7 +39,6 @@ defmodule DpulCollectionsWeb.CollectionsLiveTest do
 
       assert document
              |> Floki.find(~s{meta[property="og:title"][content="Test Project"]})
-             |> dbg()
              |> Enum.any?()
 
       assert document
