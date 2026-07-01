d7e25aad83e9437c0bc6a8ce463f984dca30c588
Trey Pendragon <tpendragon@princeton.edu>
2026-07-01 12:26:06 -0700

    Remove the mix block

--- mix.exs
+++ mix.exs
@@ -5,7 +5,6 @@ defmodule DpulCollections.MixProject do
     [
       app: :dpul_collections,
       version: "0.1.0",
-      elixir: "~> 1.19.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
