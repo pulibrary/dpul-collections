1adfb2c6dd02abafadc97d7e4e98307cb874285e
Trey Pendragon <tpendragon@princeton.edu>
2026-09-20 13:25:13 -0700

    Fix build on my desktop.

--- lib/dpul_collections/mocr/native.ex
+++ lib/dpul_collections/mocr/native.ex
@@ -4,7 +4,8 @@ defmodule DpulCollections.Mocr.Native do
     crate: "dpul_mocr",
     env: [
       {"CFLAGS", "-Wno-elaborated-enum-base"},
-      {"CXXFLAGS", "-Wno-elaborated-enum-base"}
+      {"CXXFLAGS", "-Wno-elaborated-enum-base"},
+      {"MACOSX_DEPLOYMENT_TARGET", "14.0"}
     ]
 
   def load(_model_path, _mmproj_path), do: :erlang.nif_error(:nif_not_loaded)
